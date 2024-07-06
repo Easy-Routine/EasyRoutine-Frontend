71ee5ddffd6f49f0e9fe591b9c263aa2d0b5a1c6
dhkim <dhkim@i2l.co.kr>
2024-07-06 17:26:08 +0900

    feat: theme.ts 수정 - ...commonTheme 포함 로직 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,21 @@
+export const commonTheme = {
+    primaryColor: "#40E0D0",
+    warningColor: "#FF0000",
+    subTextColor100: "#C7C7C7",
+    subTextColor200: "#A8A8A8",
+    subTextColor300: "#7D7D7D",
+    borderRadius: "16px",
+    boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25)`,
+};
+
 export const lightTheme = {
     backgroundColor: "#FFFFFF",
     mainTextColor: "#000000",
+    ...commonTheme,
 };
 
 export const darkTheme = {
     backgroundColor: "#202124",
     mainTextColor: "#FFFFFF",
-};
-
-export const commonTheme = {
-    primaryColor: "#40E0D0",
-    warningColor: "#FF0000",
-    boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25)`,
-    subTextColor100: "#C7C7C7",
-    subTextColor200: "#A8A8A8",
-    subTextColor300: "#7D7D7D",
+    ...commonTheme,
 };
