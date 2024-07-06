5d31cdeb68a84b7e43bc245ac7825812bd99cc3c
dhkim <dhkim@i2l.co.kr>
2024-07-06 14:57:07 +0900

    feat: 테마 색 및 스타일 지정

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,18 @@
 export const lightTheme = {
-    primary: "#F0F0F0",
-    secondary: "#2D2D2D",
+    backgroundColor: "#FFFFFF",
+    mainTextColor: "#000000",
 };
 
 export const darkTheme = {
-    primary: "#2D2D2D",
-    secondary: "#F0F0F0",
+    backgroundColor: "#202124",
+    mainTextColor: "#FFFFFF",
+};
+
+export const commonTheme = {
+    primaryColor: "#40E0D0",
+    warningColor: "#FF0000",
+    boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25)`,
+    subTextColor100: "#C7C7C7",
+    subTextColor200: "#A8A8A8",
+    subTextColor300: "#7D7D7D",
 };
