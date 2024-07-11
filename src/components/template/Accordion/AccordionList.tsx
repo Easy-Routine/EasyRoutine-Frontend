4f2fd7ee9e1806a5c4934368a38a19ae098128a7
dhkim <dhkim@i2l.co.kr>
2024-07-11 13:04:11 +0900

    feat: AccordionList 생성

---
+++ src/components/template/Accordion/AccordionList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+type AccordionListProps<T> = {
+    data: T[];
+    render: (value: T, key: number) => React.ReactNode;
+};
+
+const AccordionList = <T,>({ data, render }: AccordionListProps<T>) => {
+    return <Container>{data.map(render)}</Container>;
+};
+
+export default AccordionList;
