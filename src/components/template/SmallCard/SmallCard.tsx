a37c4a8adce7713dfeccbb1c6e60af3ab1fcfc20
dhkim <dhkim@i2l.co.kr>
2024-07-11 13:04:50 +0900

    feat: SmallCard 생성

---
+++ src/components/template/SmallCard/SmallCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 15px;
+`;
+const ImageBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 40px;
+    height: 40px;
+    background-color: #f0fff0;
+`;
+const ColumnBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`;
+const BoldText = styled.div`
+    font-size: 13px;
+    font-weight: 600;
+`;
+const NormalText = styled.div`
+    font-size: 13px;
+    font-weight: 400;
+    color: ${(props) => props.theme.subTextColor200};
+`;
+
+type SmallCardProps = {
+    children: React.ReactNode;
+};
+const SmallCard = ({ children }: SmallCardProps) => {
+    return <Container>{children}</Container>;
+};
+
+export default SmallCard;
+
+SmallCard.Container = Container;
+SmallCard.ImageBox = ImageBox;
+SmallCard.ColumnBox = ColumnBox;
+SmallCard.BoldText = BoldText;
+SmallCard.NormalText = NormalText;
