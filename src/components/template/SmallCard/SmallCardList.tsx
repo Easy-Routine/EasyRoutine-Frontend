16ae8953abbb661577e33d7129cae6f988c846cc
dhkim <dhkim@i2l.co.kr>
2024-07-11 13:04:58 +0900

    feat: SmallCardList 생성

---
+++ src/components/template/SmallCard/SmallCardList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    margin-top: 10px;
+`;
+
+type SmallCardListProps<T> = {
+    data: T[];
+    render: (value: T, key: number) => React.ReactNode;
+};
+
+const SmallCardList = <T,>({ data, render }: SmallCardListProps<T>) => {
+    return <Container>{data.map(render)}</Container>;
+};
+
+export default SmallCardList;
