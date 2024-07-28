73ec083fb31e056d1aa030ddb870d9ea74679c2d
dhkim <dhkim@i2l.co.kr>
2024-07-28 23:46:27 +0900

    feat: ColorTab 생성

---
+++ src/components/template/BottomBar/ColorTab.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ColorTabProvider from "context/TabContext";
+import Color from "./Color";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+`;
+
+type ColorTabProps = {
+    children: React.ReactNode;
+    defaultValue: string;
+};
+
+const ColorTab = ({ children, defaultValue }: ColorTabProps) => {
+    const [selectedValue, setSelectedValue] = useState(defaultValue);
+
+    const handleButtonClick = (value: string) => {
+        setSelectedValue(value);
+    };
+
+    useEffect(() => {
+        console.log("값변경", selectedValue);
+    }, [selectedValue]);
+
+    return (
+        <ColorTabProvider value={{ selectedValue, handleButtonClick }}>
+            <Container>{children}</Container>
+        </ColorTabProvider>
+    );
+};
+
+export default ColorTab;
+
+ColorTab.Color = Color;
+
+// 스타일과 관련된 로직과 도메인관련된 로직을 분리하자
