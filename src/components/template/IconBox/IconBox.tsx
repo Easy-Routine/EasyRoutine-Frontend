6c9e331dce229ad38c99f0b172214e7593ba2c75
dhkim <dhkim@i2l.co.kr>
2024-07-28 20:51:51 +0900

    feat: IconBox 생성

---
+++ src/components/template/IconBox/IconBox.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const IconBox = styled.div<{ width: number; height: number }>`
+    width: ${({ width }) => `${width}px`};
+    height: ${({ height }) => `${height}px`};
+    border-radius: ${({ theme }) => theme.borderRadius.circle};
+    background-color: ${({ theme }) => theme.color.primary};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export default IconBox;
