094b0cdeb7a973b32a738c2f7c40a6c38f674354
dhkim <dhkim@i2l.co.kr>
2024-07-28 22:13:14 +0900

    feat : Button 생성

---
+++ src/components/template/Button/Button.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: 100%;
+    height: 40px;
+    border: none;
+    background-color: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.text.white};
+    border-radius: ${({ theme }) => theme.borderRadius.xs};
+`;
+
+export default Button;
