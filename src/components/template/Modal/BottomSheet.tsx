c808651b1201b42103088813233b8472cb035a8e
dhkim <dhkim@i2l.co.kr>
2024-07-28 21:00:06 +0900

    fix: BottomSheet  수정 - 닫을때,  애니메이션 적용 안되는 문제 해결

--- src/components/template/Modal/BottomSheet.tsx
+++ src/components/template/Modal/BottomSheet.tsx
@@ -2,31 +2,13 @@ import { ModalContext, ModalContextType } from "context/ModalContext";
 import React, { useContext } from "react";
 import styled, { keyframes } from "styled-components";
 
-const slideIn = keyframes`
-  from {
-    transform: translateY(100%);
-  }
-  to {
-    transform: translateY(0);
-  }
-`;
-
-const slideOut = keyframes`
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(100%);
-  }
-`;
-
 const Container = styled.div<{ isOpen: boolean }>`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
     position: fixed;
-    bottom: 0%;
+    bottom: 0;
 
     background: ${({ theme }) => theme.color.background.box};
     padding: 25px;
@@ -35,8 +17,7 @@ const Container = styled.div<{ isOpen: boolean }>`
     border-top-right-radius: ${({ theme }) => theme.borderRadius.lg};
     z-index: 300;
     width: 100%;
-    animation: ${({ isOpen }) => (isOpen ? slideIn : slideOut)} 0.3s ease
-        forwards;
+    transition: all 0.5s ease-in-out;
     transform: translateY(${({ isOpen }) => (isOpen ? "0" : "100%")});
 `;
 
@@ -46,7 +27,7 @@ type BottomSheetProps = {
 
 const BottomSheet = ({ children }: BottomSheetProps) => {
     const { isOpen } = useContext(ModalContext) as ModalContextType;
-    return <> {isOpen && <Container isOpen={isOpen}>{children}</Container>}</>;
+    return <Container isOpen={isOpen}>{children}</Container>;
 };
 
 export default BottomSheet;
