9f943967aacdc00372020e3931c3aa689ebb245d
dhkim <dhkim@i2l.co.kr>
2024-08-11 23:21:22 +0900

    feat: InputGroupList 생성

--- src/components/template/InputGroup/InputGroupList.tsx
+++ src/components/template/InputGroup/InputGroupList.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-const InputGroupList = () => {
-    return <div></div>;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 10px;
+`;
+
+type InputGroupListProps<T> = {
+    data: T[];
+    render: (value: T, key: number) => React.ReactNode;
+};
+
+const InputGroupList = <T,>({ data, render }: InputGroupListProps<T>) => {
+    return <Container>{data.map(render)}</Container>;
 };
 
 export default InputGroupList;
