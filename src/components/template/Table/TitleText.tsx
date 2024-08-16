705be429fee99b34f0a789b504134fb45d768354
dhkim <dhkim@i2l.co.kr>
2024-08-16 13:00:13 +0900

    feat: TitleText 수정 - height 속성 추가

--- src/components/template/Table/TitleText.tsx
+++ src/components/template/Table/TitleText.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const TitleText = styled.div`
     font-size: ${({ theme }) => theme.fontSize.sm};
     font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    height: 22px;
 `;
 
 export default TitleText;
