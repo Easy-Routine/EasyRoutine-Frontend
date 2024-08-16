b4bfcce0ee2014e6483e4fb8d81851d0584347af
dhkim <dhkim@i2l.co.kr>
2024-08-16 12:59:16 +0900

    feat: OrderText 수정 - display 속성 추가

--- src/components/template/Table/OrderText.tsx
+++ src/components/template/Table/OrderText.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
 const OrderText = styled.div`
+    display: flex;
+    align-items: center;
+    
     font-size: ${({ theme }) => theme.fontSize.sm};
     font-weight: ${({ theme }) => theme.fontWeight.regular};
+    
+    height: 19px;
 `;
 
 export default OrderText;
