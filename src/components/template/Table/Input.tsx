1b170440c3a5bb79552fd2b7499358c245948cc5
dhkim <dhkim@i2l.co.kr>
2024-08-16 12:57:58 +0900

    feat: Input 수정 - height 속성 추가 - box-sizing 속성 추가 - z-index 속성 추가

--- src/components/template/Table/Input.tsx
+++ src/components/template/Table/Input.tsx
@@ -2,16 +2,20 @@ import React, { ChangeEvent, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.input<{ width: number }>`
-    width: ${({ width }) => `${width}px`};  
+    width: ${({ width }) => `${width}px`};
+    height: 22px;
     outline: none;
     border: none;
     border-bottom: 2px solid ${({ theme }) => theme.color.gray.light};
+    box-sizing: border-box;
     background-color: inherit;
     font-size: ${({ theme }) => theme.fontSize.sm};
     font-weight: ${({ theme }) => theme.fontWeight.regular};
     &:focus {
         border-color: ${({ theme }) => theme.color.primary};
     }
+
+    z-index: 1000;
     
 `;
 
@@ -21,7 +25,7 @@ type InputProps = {
     onInputChange: (value: string) => void;
 };
 
-const Input = ({ width = 16, value, onInputChange }: InputProps) => {
+const Input = ({ width = 18, value, onInputChange }: InputProps) => {
     const [inputValue, setInputValue] = useState(value);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
