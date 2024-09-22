fff7892bbb90270ea6afc822e6ff89f379237d70
dhkim <dhkim@i2l.co.kr>
2024-09-22 23:19:45 +0900

    remove: 사용하지 않는 파일 전부 삭제

--- src/components/template/ProgressForm/Text.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Text = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.sm};
-    font-weight: ${({ theme }) => theme.fontWeight.regular};
-`;
-
-export default Text;
