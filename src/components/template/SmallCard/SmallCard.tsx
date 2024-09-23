6fa9118f09d5cc105aa79dddba0506ba788ce364
dhkim <dhkim@i2l.co.kr>
2024-09-24 08:50:09 +0900

    fix: SmallCard 컴포넌트 수정 - 스타일 수정

--- src/components/template/SmallCard/SmallCard.tsx
+++ src/components/template/SmallCard/SmallCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
+    width: 100%;
     display: flex;
     align-items: center;
     gap: 15px;
--- src/components/template/SmallCard/SmallCardList.tsx
+++ src/components/template/SmallCard/SmallCardList.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
+    width: 100%;
     display: flex;
     flex-direction: column;
     gap: 15px;
