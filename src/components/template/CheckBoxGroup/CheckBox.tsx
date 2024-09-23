487fde1f35614ce2eb2b7798e9e7bc0025c70bb6
dhkim <dhkim@i2l.co.kr>
2024-09-24 08:49:03 +0900

    fix: CheckBoxGroup 컴포넌트 수정 - 구조 수정, ContextAPI => HookAPI - CheckBoxGroup.Wrapper 컴포넌트 생성

--- src/components/template/CheckBoxGroup/CheckBox.tsx
+++ src/components/template/CheckBoxGroup/CheckBox.tsx
@@ -1,31 +1,41 @@
-import { CheckBoxGroupContext, CheckBoxGroupContextType } from 'context/CheckBoxGroupContext';
-import React, { useContext } from 'react';
-import styled from 'styled-components';
-import { ReactComponent as CheckIcon } from 'assets/image/check.svg';
+import styled from "styled-components";
+import { ReactComponent as CheckIcon } from "assets/image/check.svg";
 
 const Container = styled.div<{ isSelected: boolean }>`
-    width: 20px;
+    width: 21px;
     height: 20px;
     border-radius: ${({ theme }) => theme.borderRadius.xs};
-    border: 1px solid ${({ theme }) => theme.color.gray.light};
-    background-color: ${({ theme, isSelected }) => (isSelected ? theme.color.primary : theme.color.background.box)};
+    border: 1px solid
+        ${({ theme, isSelected }) =>
+            isSelected ? "none" : theme.color.gray.light};
+    background-color: ${({ theme, isSelected }) =>
+        isSelected ? theme.color.primary : theme.color.background.box};
     display: flex;
     justify-content: center;
     align-items: center;
+    box-sizing: border-box;
 `;
 
 type CheckBoxProps = {
     value: string;
+    selectedValues: string[];
+    onCheckBoxClick: (value: string) => void;
 };
 
-const CheckBox = ({ value }: CheckBoxProps) => {
+const CheckBox = ({
+    value,
+    selectedValues,
+    onCheckBoxClick,
+}: CheckBoxProps) => {
     // 버튼을 클릭하면, 선택상태가 변경된다.
-    const { handleButtonClick, selectedValues } = useContext(CheckBoxGroupContext) as CheckBoxGroupContextType;
 
     const isSelected = selectedValues.includes(value);
 
     return (
-        <Container isSelected={isSelected} onClick={() => handleButtonClick(value)}>
+        <Container
+            isSelected={isSelected}
+            onClick={() => onCheckBoxClick(value)}
+        >
             {isSelected && <CheckIcon width={12.5} />}
         </Container>
     );
