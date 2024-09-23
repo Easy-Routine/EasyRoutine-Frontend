82f7e056734071a8a62d32124433620700a0e45b
dhkim <dhkim@i2l.co.kr>
2024-09-24 08:49:19 +0900

    feat: useCheckBox 훅 생성

---
+++ src/hooks/client/useCheckBox.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useCheckBox = () => {
+    const [selectedValues, setSelectedValues] = useState<string[]>([]);
+
+    const handleCheckBoxClick = (value: string) => {
+        const newState = structuredClone(selectedValues);
+        if (selectedValues.includes(value)) {
+            setSelectedValues(
+                newState.filter((item: string) => item !== value)
+            );
+        } else {
+            newState.push(value);
+            setSelectedValues(newState);
+        }
+    };
+
+    useEffect(() => {
+        console.log("값변경", selectedValues);
+    }, [selectedValues]);
+
+    return { selectedValues, handleCheckBoxClick };
+};
+
+export default useCheckBox;
