9811d804c522d9c70d2bcc6e1abd3578c5a0e7e5
dhkim <dhkim@i2l.co.kr>
2024-09-24 20:00:20 +0900

    feat: EmptyView 컴포넌트 생성

---
+++ src/components/content/EmptyView/EmptyView.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as EmptyImage } from "assets/image/empty.svg";
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    font-size: ${({ theme }) => theme.fontSize.sm};
+    font-weight: ${({ theme }) => theme.fontWeight.regular};
+    color: ${({ theme }) => theme.color.gray.normal};
+`;
+
+type EmptyViewProps = {
+    emptyText: string;
+};
+
+const EmptyView = ({ emptyText }: EmptyViewProps) => {
+    return (
+        <Container>
+            <EmptyImage />
+            {emptyText}
+        </Container>
+    );
+};
+
+export default EmptyView;
