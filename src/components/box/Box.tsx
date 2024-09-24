fa9bac924cbc7e3254dd2882a52870f2c3fc050d
dhkim <dhkim@i2l.co.kr>
2024-09-24 11:19:02 +0900

    feat: Box 컴포넌트 생성

---
+++ src/components/box/Box.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div<{ width?: string }>`
+    background-color: ${({ theme }) => theme.color.background.box};
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
+    width: ${({ width }) => width};
+`;
+
+type BoxProps = {
+    children: React.ReactNode;
+    width?: string;
+};
+
+const Box = ({ children, width }: BoxProps) => {
+    return <Container width={width}>{children}</Container>;
+};
+
+export default Box;
