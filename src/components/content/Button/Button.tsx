11017929b51e768cc7ffa4b4d8c50b470220942c
dhkim <dhkim@i2l.co.kr>
2024-09-24 14:47:45 +0900

    style: Button 컴포넌트 스타일 수정 - 최소 높이 값 지정

--- src/components/content/Button/Button.tsx
+++ src/components/content/Button/Button.tsx
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Button = styled.button`
     width: 100%;
     height: 40px;
+    min-height: 40px;
     border: none;
     background-color: ${({ theme }) => theme.color.primary};
     color: ${({ theme }) => theme.color.text.white};
