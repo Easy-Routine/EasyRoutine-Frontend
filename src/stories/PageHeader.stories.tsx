016194ddd88116f2aa498a44e23f457a17672829
dhkim <dhkim@i2l.co.kr>
2024-09-24 16:59:10 +0900

    feat: PageHeader 스토리 생성

---
+++ src/stories/PageHeader.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PageHeader from "components/content/PageHeader/PageHeader";
+
+const meta: Meta<any> = {};
+
+export default meta;
+
+type Story = StoryObj<any>;
+
+export const Default: Story = {
+    render: () => {
+        return (
+            <PageHeader>
+                <PageHeader.ReturnCircle />
+                <PageHeader.PageTitle>루틴 생성</PageHeader.PageTitle>
+            </PageHeader>
+        );
+    },
+};
