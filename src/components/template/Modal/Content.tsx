3f74d451ee659e328a9d260f864ceba96bc6cef4
dhkim <dhkim@i2l.co.kr>
2024-09-24 11:40:41 +0900

    fix: Modal 컴포넌트 수정 - 스타일 수정, 커스텀 스타일 롤백

--- src/components/template/Modal/Content.tsx
+++ src/components/template/Modal/Content.tsx
@@ -1,36 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
-type ContentProps = {
-    children: React.ReactNode;
-    isOpen: boolean;
-};
-
 const Container = styled.div`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    background: ${({ theme }) => theme.color.background.box};
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
     z-index: 200;
-    width: 100%;
+    width: 80%;
 `;
+
+type ContentProps = {
+    children: React.ReactNode;
+    isOpen: boolean;
+};
 
 const Content = ({ children, isOpen }: ContentProps) => {
     return <>{isOpen && <Container>{children}</Container>}</>;
 };
 
 export default Content;
-
-/*
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    background: ${({ theme }) => theme.color.background.box};
-    padding: 25px;
-    box-sizing: border-box;
-    border-radius: ${({ theme }) => theme.borderRadius.lg};
-    height: 300px;
-    width: 80%;
-*/
