39c82807027cbb69f429ba53dad770f0d8d1db5a
dhkim <dhkim@i2l.co.kr>
2024-09-24 19:25:51 +0900

    feat: LabelBox 스토리 생성

---
+++ src/stories/LabelBox.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import LabelBox from "components/box/LabelBox/LabelBox";
+import ChipTab from "components/content/ChipTab/ChipTab";
+import useTab from "hooks/client/useTab";
+
+type LabelBoxProps = React.ComponentProps<typeof LabelBox>;
+
+const meta: Meta<LabelBoxProps> = {
+    component: LabelBox,
+};
+
+export default meta;
+
+type Story = StoryObj<any>;
+
+export const DefaultLabelBox: Story = {
+    render: () => {
+        const { selectedValue, handleTabClick } = useTab("가슴");
+        return (
+            <LabelBox labelText="운동 부위">
+                <ChipTab>
+                    <ChipTab.Chip
+                        value="가슴"
+                        selectedValue={selectedValue}
+                        onTabClick={handleTabClick}
+                    >
+                        가슴
+                    </ChipTab.Chip>
+                    <ChipTab.Chip
+                        value="등"
+                        selectedValue={selectedValue}
+                        onTabClick={handleTabClick}
+                    >
+                        등
+                    </ChipTab.Chip>
+                    <ChipTab.Chip
+                        value="어깨"
+                        selectedValue={selectedValue}
+                        onTabClick={handleTabClick}
+                    >
+                        어깨
+                    </ChipTab.Chip>
+                    <ChipTab.Chip
+                        value="하체"
+                        selectedValue={selectedValue}
+                        onTabClick={handleTabClick}
+                    >
+                        하체
+                    </ChipTab.Chip>
+                    <ChipTab.Chip
+                        value="팔"
+                        selectedValue={selectedValue}
+                        onTabClick={handleTabClick}
+                    >
+                        팔
+                    </ChipTab.Chip>
+                    <ChipTab.Chip
+                        value="기타"
+                        selectedValue={selectedValue}
+                        onTabClick={handleTabClick}
+                    >
+                        기타
+                    </ChipTab.Chip>
+                </ChipTab>
+            </LabelBox>
+        );
+    },
+};
