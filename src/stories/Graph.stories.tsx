e9ec93438bbcb6c41e442f453e29b8d220b94c54
dhkim <dhkim@i2l.co.kr>
2024-09-25 13:02:13 +0900

    feat: Graph 스토리 생성

--- src/stories/Graph.stories.tsx
+++ src/stories/Graph.stories.tsx
@@ -14,9 +14,29 @@ type Story = StoryObj<any>;
 
 export const WithGraph: Story = {
     render: () => {
+        const data = [
+            { month: "1월", value: 400 },
+            { month: "2월", value: 300 },
+            { month: "3월", value: 200 },
+            { month: "4월", value: 278 },
+            { month: "5월", value: 189 },
+            { month: "6월", value: 400 },
+            { month: "7월", value: 300 },
+            { month: "8월", value: 400 },
+            { month: "9월", value: 300 },
+            { month: "10월", value: 200 },
+            { month: "11월", value: 278 },
+            { month: "12월", value: 189 },
+        ];
+
         return (
             <Box>
-                <Graph />
+                <Graph
+                    onDotClick={(data) => console.log(data)}
+                    data={data}
+                    lineKey="month"
+                    areaKey="value"
+                />
             </Box>
         );
     },
