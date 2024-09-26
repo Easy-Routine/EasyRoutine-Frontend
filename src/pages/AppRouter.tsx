b77cd3e24801b26764d885ca64fcedec21327f75
dhkim <dhkim@i2l.co.kr>
2024-09-26 20:31:03 +0900

    feat: AppRouter 컴포넌트 - PageTemplate 컴포넌트 아웃렛 적용

--- src/pages/AppRouter.tsx
+++ src/pages/AppRouter.tsx
@@ -1,38 +1,44 @@
+import PageTemplate from "components/box/PageTemplate/PageTemplate";
 import ROUTES from "constants/routes";
 import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route
-                path={ROUTES.LOGIN.PATH}
-                element={<ROUTES.LOGIN.COMPONENT />}
-            />
-            <Route
-                path={ROUTES.CONFIG.LIST.PATH}
-                element={<ROUTES.CONFIG.LIST.COMPONENT />}
-            />
-            <Route
-                path={ROUTES.CONFIG.DETAIL.PATH}
-                element={<ROUTES.CONFIG.DETAIL.COMPONENT />}
-            />
-            <Route
-                path={ROUTES.RECORD.LIST.PATH}
-                element={<ROUTES.RECORD.LIST.COMPONENT />}
-            />
-            <Route
-                path={ROUTES.RECORD.DETAIL.PATH}
-                element={<ROUTES.RECORD.DETAIL.COMPONENT />}
-            />
-            <Route
-                path={ROUTES.PROGRESS.PATH}
-                element={<ROUTES.PROGRESS.COMPONENT />}
-            />
-            <Route
-                path={ROUTES.LIBRARY.PATH}
-                element={<ROUTES.LIBRARY.COMPONENT />}
-            />
-            <Route path={ROUTES.MY.PATH} element={<ROUTES.MY.COMPONENT />} />
+            <Route element={<PageTemplate />}>
+                <Route
+                    path={ROUTES.LOGIN.PATH}
+                    element={<ROUTES.LOGIN.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.CONFIG.LIST.PATH}
+                    element={<ROUTES.CONFIG.LIST.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.CONFIG.DETAIL.PATH}
+                    element={<ROUTES.CONFIG.DETAIL.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.RECORD.LIST.PATH}
+                    element={<ROUTES.RECORD.LIST.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.RECORD.DETAIL.PATH}
+                    element={<ROUTES.RECORD.DETAIL.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.PROGRESS.PATH}
+                    element={<ROUTES.PROGRESS.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.LIBRARY.PATH}
+                    element={<ROUTES.LIBRARY.COMPONENT />}
+                />
+                <Route
+                    path={ROUTES.MY.PATH}
+                    element={<ROUTES.MY.COMPONENT />}
+                />
+            </Route>
         </Routes>
     );
 };
