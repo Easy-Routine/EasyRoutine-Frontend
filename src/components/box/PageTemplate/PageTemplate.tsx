e8a7efe60e83f81d070d7cdfdf358c2fe01985d9
dhkim <dhkim@i2l.co.kr>
2024-09-26 20:30:09 +0900

    feat: PageTemplate 컴포넌트 생성

---
+++ src/components/box/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-top: 20px;
+`;
+
+const PageTemplate = () => {
+    return (
+        <Container>
+            <Outlet />
+        </Container>
+    );
+};
+
+export default PageTemplate;
