d3ca826a9619b7d5fb579114ee75e0567d497466
dhkim <dhkim@i2l.co.kr>
2024-09-27 16:55:24 +0900

    feat: ColorTabBottomBar 컴포넌트 생성

---
+++ src/components/business/ColorTabBottomBar.tsx
@@ -0,0 +1,50 @@
+import BottomBar from "components/box/BottomBar/BottomBar";
+import ColorTab from "components/box/BottomBar/ColorTab";
+import useTab from "hooks/client/useTab";
+import React from "react";
+
+type ColorTabBottomBarProps = {
+    defaultValue: string;
+};
+
+const ColorTabBottomBar = ({ defaultValue }: ColorTabBottomBarProps) => {
+    const { selectedValue, handleTabClick } = useTab(defaultValue);
+    return (
+        <BottomBar>
+            <ColorTab>
+                <ColorTab.Color
+                    selectedValue={selectedValue}
+                    onTabClick={handleTabClick}
+                    value="violet"
+                    backgroundColor="#855CF8"
+                />
+                <ColorTab.Color
+                    selectedValue={selectedValue}
+                    onTabClick={handleTabClick}
+                    value="orange"
+                    backgroundColor="#F26B2C"
+                />
+                <ColorTab.Color
+                    selectedValue={selectedValue}
+                    onTabClick={handleTabClick}
+                    value="green"
+                    backgroundColor="#2DAF2D"
+                />
+                <ColorTab.Color
+                    selectedValue={selectedValue}
+                    onTabClick={handleTabClick}
+                    value="blue"
+                    backgroundColor="#455A64"
+                />
+                <ColorTab.Color
+                    selectedValue={selectedValue}
+                    onTabClick={handleTabClick}
+                    value="pink"
+                    backgroundColor="#DD408F"
+                />
+            </ColorTab>
+        </BottomBar>
+    );
+};
+
+export default ColorTabBottomBar;
