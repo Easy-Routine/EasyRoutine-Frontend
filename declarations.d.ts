72bc1d00ca190c09807e03f1cb88fae9e0c7d3a3
dhkim <dhkim@i2l.co.kr>
2024-09-27 12:30:13 +0900

    feat: declarations 타입 - ttf 파일 타입 추가

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,11 +1,18 @@
-declare module '*.svg' {
-    import * as React from 'react';
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+declare module "*.svg" {
+    import * as React from "react";
+    export const ReactComponent: React.FunctionComponent<
+        React.SVGProps<SVGSVGElement>
+    >;
     const src: string;
     export default src;
 }
 
-declare module '*.png' {
+declare module "*.png" {
     const value: any;
     export = value;
+}
+
+declare module "*.ttf" {
+    const src: string;
+    export default src;
 }
