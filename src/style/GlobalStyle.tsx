ea7d4885dbbc478890ea6075bd2943335cdacfcd
dhkim <dhkim@i2l.co.kr>
2024-09-27 12:31:31 +0900

    feat: GlobalStyle 컴포넌트 - Noto Sans KR 폰트 기본 적용

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,10 +1,41 @@
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import NotoSansRegular from "assets/fonts/NotoSansKR-Regular.ttf";
+import NotoSansBold from "assets/fonts/NotoSansKR-Bold.ttf";
+import NotoSansSemiBold from "assets/fonts/NotoSansKR-SemiBold.ttf";
+
+const fontPath = process.env;
+
+console.log(fontPath, "fontPath");
 
 export const GlobalStyle = createGlobalStyle`
 	${reset}
-	body{
-		background:${(props) => props.theme.color.background.page};
+	@font-face {
+		font-family: 'Noto Sans Korean';
+		src: url(${NotoSansRegular}) format('truetype');
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	@font-face {
+		font-family: 'Noto Sans Korean';
+		src: url(${NotoSansBold}) format('truetype');
+		font-weight: 600;
+		font-style: normal;
+	}
+
+	@font-face {
+		font-family: 'Noto Sans Korean';
+		src: url(${NotoSansSemiBold}) format('truetype');
+		font-weight: 700; // ExtraBold
+		font-style: normal;
+	}
+
+
+
+	body {
+		background: ${(props) => props.theme.color.background.page};
         color: ${(props) => props.theme.color.text.black};
+		font-family: 'Noto Sans Korean', sans-serif;
 	}
 `;
