c9e762830462c65673c6662bcada222fac00cff7
dhkim <dhkim@i2l.co.kr>
2024-09-29 23:04:57 +0900

    feat: WorkoutLibraryPage 컴포넌트 - NavigationBottomBar 추가

--- src/pages/WorkoutLibraryPage.tsx
+++ src/pages/WorkoutLibraryPage.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import NavigationBottomBar from "components/business/NavigationBottomBar/NavigationBottomBar";
+import ROUTES from "constants/routes";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 const WorkoutLibraryPage = () => {
-    return <div>라이브러리 페이지</div>;
+    return (
+        <Container>
+            라이브러리 페이지
+            <NavigationBottomBar defaultValue={ROUTES.LIBRARY.PATH} />
+        </Container>
+    );
 };
 
 export default WorkoutLibraryPage;
