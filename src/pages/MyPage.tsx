bfd803bbc17699854f8a462a88f8c5f8983922d7
dhkim <dhkim@i2l.co.kr>
2024-09-29 23:04:40 +0900

    feat: MyPage 컴포넌트 - NavigationBottomBar 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import NavigationBottomBar from "components/business/NavigationBottomBar/NavigationBottomBar";
+import ROUTES from "constants/routes";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 const MyPage = () => {
-    return <div>마이 페이지</div>;
+    return (
+        <Container>
+            마이 페이지
+            <NavigationBottomBar defaultValue={ROUTES.MY.PATH} />
+        </Container>
+    );
 };
 
 export default MyPage;
