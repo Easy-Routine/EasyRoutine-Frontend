4a372d0a6094da5f003d2a0438f574c454b03386
dhkim <dhkim@i2l.co.kr>
2024-09-29 23:02:55 +0900

    feat: RoutineRecordTabView 컴포넌트 생성

---
+++ src/components/business/RoutineRecordTabView.tsx
@@ -0,0 +1,52 @@
+import Tab from "components/content/Tab/Tab";
+import useTab from "hooks/client/useTab";
+import styled from "styled-components";
+import RoutineCalendarView from "./RoutineCalendarView";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+// viewMapper의 타입 정의
+type ViewMapper = {
+    calendar: () => JSX.Element;
+    graph: () => JSX.Element;
+};
+
+const RoutineRecordTabView = () => {
+    const viewMapper: ViewMapper = {
+        calendar: RoutineCalendarView,
+        graph: () => <div>Graph View</div>, // TODO: Add Graph View component here
+    };
+
+    const { selectedValue, handleTabClick } = useTab("calendar");
+
+    // 렌더링할 컴포넌트를 선택
+    const SelectedView = viewMapper[selectedValue as keyof ViewMapper];
+
+    return (
+        <Container>
+            <Tab>
+                <Tab.Button
+                    value="calendar"
+                    selectedValue={selectedValue}
+                    onButtonClick={handleTabClick}
+                >
+                    캘린더
+                </Tab.Button>
+                <Tab.Button
+                    value="graph"
+                    selectedValue={selectedValue}
+                    onButtonClick={handleTabClick}
+                >
+                    통계
+                </Tab.Button>
+            </Tab>
+            <SelectedView />
+        </Container>
+    );
+};
+
+export default RoutineRecordTabView;
