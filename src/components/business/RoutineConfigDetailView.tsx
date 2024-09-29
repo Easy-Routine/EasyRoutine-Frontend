066dd47d5d17e61095d89c81ee686e568ef2c158
dhkim <dhkim@i2l.co.kr>
2024-09-29 16:40:26 +0900

    feat: RoutineConfigDetailView 컴포넌트 생성

---
+++ src/components/business/RoutineConfigDetailView.tsx
@@ -0,0 +1,167 @@
+import Accordion from "components/box/Accordion/Accordion";
+import Box from "components/box/Box/Box";
+import TitleText from "components/content/TitleText/TitleText";
+import WorkoutConfigAccordion from "./WorkoutConfigAccordion";
+import { RoutineConfig } from "types/config";
+import SeatedRowImage from "assets/image/seated-row.png";
+import RoutineConfigColorTabBottomBar from "./RoutineConfigColorTabBottomBar";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+const RoutineConfigDetailView = () => {
+    const data: RoutineConfig = {
+        id: 1,
+        name: "나의 가슴 루틴",
+        color: "orange",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: 3,
+        workoutConfigs: [
+            {
+                id: 1,
+                name: "벤치프레스",
+                workoutImage: SeatedRowImage,
+                type: "weight, time",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                routineConfigId: 1,
+                setConfigs: [
+                    {
+                        id: 1,
+                        order: 1,
+                        weight: 10,
+                        rep: 10,
+                        restSec: 50,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                    {
+                        id: 2,
+                        order: 2,
+                        weight: 15,
+                        rep: 8,
+                        restSec: 30,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                    {
+                        id: 3,
+                        order: 3,
+                        weight: 20,
+                        rep: 6,
+                        restSec: 10,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                ],
+            },
+            {
+                id: 2,
+                name: "벤치프레스",
+                workoutImage: SeatedRowImage,
+                type: "weight, time",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                routineConfigId: 1,
+                setConfigs: [
+                    {
+                        id: 1,
+                        order: 1,
+                        weight: 10,
+                        rep: 10,
+                        restSec: 50,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                    {
+                        id: 2,
+                        order: 2,
+                        weight: 15,
+                        rep: 8,
+                        restSec: 30,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                    {
+                        id: 3,
+                        order: 3,
+                        weight: 20,
+                        rep: 6,
+                        restSec: 10,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                ],
+            },
+            {
+                id: 3,
+                name: "벤치프레스",
+                workoutImage: SeatedRowImage,
+                type: "weight, time",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                routineConfigId: 1,
+                setConfigs: [
+                    {
+                        id: 1,
+                        order: 1,
+                        weight: 10,
+                        rep: 10,
+                        restSec: 50,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                    {
+                        id: 2,
+                        order: 2,
+                        weight: 15,
+                        rep: 8,
+                        restSec: 30,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                    {
+                        id: 3,
+                        order: 3,
+                        weight: 20,
+                        rep: 6,
+                        restSec: 10,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        workoutConfigId: 1,
+                    },
+                ],
+            },
+        ],
+    };
+
+    return (
+        <Container>
+            <Box>
+                <TitleText>월요일 루틴</TitleText>
+            </Box>
+            <Accordion.List
+                data={data.workoutConfigs}
+                render={(workoutConfig) => (
+                    <WorkoutConfigAccordion data={workoutConfig} />
+                )}
+            />
+            <RoutineConfigColorTabBottomBar defaultValue={data.color} />
+        </Container>
+    );
+};
+
+export default RoutineConfigDetailView;
