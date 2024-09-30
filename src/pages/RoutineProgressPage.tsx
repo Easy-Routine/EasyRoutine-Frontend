e4c12a6d946f387c40b1e0efea6a5695c2ea0834
dhkim <dhkim@i2l.co.kr>
2024-10-01 01:36:00 +0900

    feat: RoutineProgressPage 컴포넌트 - ReturnPageHeader 컴포넌트 추가 - RoutineProgressListView 컴포넌트 추가

--- src/pages/RoutineProgressPage.tsx
+++ src/pages/RoutineProgressPage.tsx
@@ -1,7 +1,21 @@
+import ReturnPageHeader from "components/business/ReturnPageHeader";
+import RoutineProgressListView from "components/business/RoutineProgressListView";
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
 
 const RoutineProgressPage = () => {
-    return <div>진행 페이지</div>;
+    return (
+        <Container>
+            <ReturnPageHeader pageTitleText="운동 진행" />
+            <RoutineProgressListView />
+        </Container>
+    );
 };
 
 export default RoutineProgressPage;
