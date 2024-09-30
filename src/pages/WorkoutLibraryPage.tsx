8eac8cd9ae70144056a1fe80f0e90961e86ee5cf
dhkim <dhkim@i2l.co.kr>
2024-09-30 16:14:55 +0900

    feat: WorkoutLibraryPage 컴포넌트 - Logo 컴포넌트 추가 - WorkoutLibraryListView 컴포넌트 추가

--- src/pages/WorkoutLibraryPage.tsx
+++ src/pages/WorkoutLibraryPage.tsx
@@ -1,13 +1,20 @@
 import NavigationBottomBar from "components/business/NavigationBottomBar/NavigationBottomBar";
+import WorkoutLibraryListView from "components/business/WorkoutLibraryListView";
+import Logo from "components/content/Logo/Logo";
 import ROUTES from "constants/routes";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
 
 const WorkoutLibraryPage = () => {
     return (
         <Container>
-            라이브러리 페이지
+            <Logo />
+            <WorkoutLibraryListView />
             <NavigationBottomBar defaultValue={ROUTES.LIBRARY.PATH} />
         </Container>
     );
