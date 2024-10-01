287fc93cbaf4b9dcbd482772c6f40f183f380306
dhkim <dhkim@i2l.co.kr>
2024-10-01 18:43:28 +0900

    feat: MyPage 컴포넌트 - UnderlineBox 컴포넌트 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,14 +1,54 @@
 import NavigationBottomBar from "components/business/NavigationBottomBar/NavigationBottomBar";
+import Logo from "components/content/Logo/Logo";
+import UnderlineBox from "components/content/UnderlineBox/UnderlineBox";
 import ROUTES from "constants/routes";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
+import { ReactComponent as MoonIcon } from "assets/image/moon.svg";
+import { ReactComponent as HumanIcon } from "assets/image/human2.svg";
+import Toggle from "components/content/Toggle/Toggle";
+import { useContext } from "react";
+import { ThemeContext, ThemeContextType } from "context/ThemeContext";
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+const UnderlineBoxWrapper = styled.div``;
 
 const MyPage = () => {
+    const { themeMode, setThemeMode } = useContext(
+        ThemeContext
+    ) as ThemeContextType;
+
+    const handleToggleButtonClick = () => {
+        setThemeMode(themeMode === "dark" ? "light" : "dark");
+    };
+
     return (
         <Container>
-            마이 페이지
+            <Logo />
             <NavigationBottomBar defaultValue={ROUTES.MY.PATH} />
+
+            <UnderlineBoxWrapper>
+                <UnderlineBox>
+                    <UnderlineBox.TitleWrapper>
+                        <MoonIcon />
+                        다크모드 설정
+                    </UnderlineBox.TitleWrapper>
+                    <Toggle
+                        defaultValue={themeMode === "dark"}
+                        onToggleChange={handleToggleButtonClick}
+                    />
+                </UnderlineBox>
+                <UnderlineBox>
+                    <UnderlineBox.TitleWrapper>
+                        <HumanIcon />
+                        문의 사항
+                    </UnderlineBox.TitleWrapper>
+                </UnderlineBox>
+            </UnderlineBoxWrapper>
         </Container>
     );
 };
