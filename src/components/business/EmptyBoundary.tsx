8538248edebee6591775e7109a58eddc8dc9f439
dhkim <dhkim@i2l.co.kr>
2024-10-02 09:52:25 +0900

    feat: EmptyBoundary 컴포넌트 생성

---
+++ src/components/business/EmptyBoundary.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type EmptyBoundaryProps = {
+    data: any;
+    fallback: React.ReactNode;
+    children: React.ReactNode;
+};
+
+const EmptyBoundary = ({ data, fallback, children }: EmptyBoundaryProps) => {
+    if (data.length === 0) {
+        return <>{fallback}</>;
+    }
+
+    return <>{children}</>;
+};
+
+export default EmptyBoundary;
