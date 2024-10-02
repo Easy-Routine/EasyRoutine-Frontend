89b7505c6fe9a74f85dc7d8530b0ca6d119c110f
dhkim <dhkim@i2l.co.kr>
2024-10-02 09:53:25 +0900

    feat: RoutineConfigFloatingActionButton 컴포넌트 - EmptyBoundary 컴포넌트 추가

--- src/components/business/RoutineConfigFloatingActionButton.tsx
+++ src/components/business/RoutineConfigFloatingActionButton.tsx
@@ -2,15 +2,37 @@ import FloatingActionButton from "components/content/FloatingActionButton/Floati
 import ROUTES from "constants/routes";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import EmptyBoundary from "./EmptyBoundary";
+import styled from "styled-components";
+import { RoutineConfig } from "types/config";
 
 const RoutineConfigFloatingActionButton = () => {
+    // TODO : 데이터 페칭
+    const data: RoutineConfig[] | {} = [{}];
+
     const navigate = useNavigate();
     const handleButtonClick = async () => {
         // TODO: routineConfig 데이터 생성
         navigate(ROUTES.CONFIG.DETAIL.PATH("1"));
     };
 
-    return <FloatingActionButton onButtonClick={handleButtonClick} />;
+    return (
+        <EmptyBoundary
+            data={data}
+            fallback={
+                <FloatingActionButton
+                    onButtonClick={handleButtonClick}
+                    text={
+                        <>
+                            본인 만의 루틴이 있나요? <br /> 루틴을 생성해보세요!
+                        </>
+                    }
+                />
+            }
+        >
+            <FloatingActionButton onButtonClick={handleButtonClick} />
+        </EmptyBoundary>
+    );
 };
 
 export default RoutineConfigFloatingActionButton;
