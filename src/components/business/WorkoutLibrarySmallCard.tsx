38b2943deb967709d2c6d010178bb9def9d1bce5
dhkim <dhkim@i2l.co.kr>
2024-10-03 00:39:39 +0900

    feat: WorkoutLibrarySmallCard 컴포넌트 생성

---
+++ src/components/business/WorkoutLibrarySmallCard.tsx
@@ -0,0 +1,40 @@
+import SmallCard from "components/content/SmallCard/SmallCard";
+import { WorkoutLibrary } from "types/workout-library";
+import SeatedRowImage from "assets/image/seated-row.png";
+import styled from "styled-components";
+import { ReactComponent as ArrowIcon } from "assets/image/arrow.svg";
+
+const RightArrowIcon = styled(ArrowIcon)`
+    transform: rotate(-90deg);
+`;
+
+type WorkoutLibrarySmallCardProps = {
+    data: WorkoutLibrary;
+    onSmallCardClick: (workoutLibraryId: string) => void;
+    onSmallCardLongPress: (workoutLibraryId: string) => void;
+};
+
+const WorkoutLibrarySmallCard = ({
+    data,
+    onSmallCardClick,
+    onSmallCardLongPress,
+}: WorkoutLibrarySmallCardProps) => {
+    return (
+        <SmallCard
+            onCardClick={() => onSmallCardClick(data.id)}
+            onLongPress={() => onSmallCardLongPress(data.id)}
+        >
+            <SmallCard.Between>
+                <SmallCard.ImageText>
+                    <SmallCard.ImageBox>
+                        <img src={SeatedRowImage} alt="seated row" />
+                    </SmallCard.ImageBox>
+                    <SmallCard.NormalText>{data.name}</SmallCard.NormalText>
+                </SmallCard.ImageText>
+                <RightArrowIcon />
+            </SmallCard.Between>
+        </SmallCard>
+    );
+};
+
+export default WorkoutLibrarySmallCard;
