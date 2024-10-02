35e49aaca20926ac0ca726b5d2f8fe36f0852e03
dhkim <dhkim@i2l.co.kr>
2024-10-02 13:46:18 +0900

    feat: RoutineRecordAccordion 컴포넌트 - RoutineDeleteModal 생성 및 추가

--- src/components/business/RoutineRecordAccordion.tsx
+++ src/components/business/RoutineRecordAccordion.tsx
@@ -6,12 +6,16 @@ import { RoutineRecord } from "types/recrod";
 import { ReactComponent as ArrowIcon } from "assets/image/arrow.svg";
 import { ReactComponent as PenIcon } from "assets/image/pen.svg";
 import { ReactComponent as RunIcon } from "assets/image/run.svg";
+import { ReactComponent as TrashIcon } from "assets/image/trash.svg";
 import SmallCardList from "components/content/SmallCard/SmallCardList";
 import SmallCard from "components/content/SmallCard/SmallCard";
 import IconTextBox from "components/content/IconTextBox/IconTextBox";
 import { useNavigate } from "react-router-dom";
 import ROUTES from "constants/routes";
 import { useTheme } from "styled-components";
+import useModal from "hooks/client/useModal";
+import Modal from "components/box/Modal/Modal";
+import Confirm from "components/content/Confirm/Confirm";
 
 const RoutineRecordAccordion = ({ data }: { data: RoutineRecord }) => {
     const navigate = useNavigate();
@@ -20,9 +24,11 @@ const RoutineRecordAccordion = ({ data }: { data: RoutineRecord }) => {
     const { isOpen, handleToggleAccordion, handleDragEnd, opacity, x } =
         useAccordion();
 
-    const handleRoutineRecordDetailClick = (routineRecordId: string) => {
-        navigate(ROUTES.RECORD.DETAIL.PATH(routineRecordId));
-    };
+    const {
+        isOpen: isRoutineDeleteModalOpen,
+        handleOpenModal: openRoutineDeleteModal,
+        handleCloseModal: closeRoutineDeleteModal,
+    } = useModal();
 
     return (
         <Accordion>
@@ -71,22 +77,77 @@ const RoutineRecordAccordion = ({ data }: { data: RoutineRecord }) => {
                         <IconTextBox.IconText
                             color={color.gray.dark}
                             onIconTextClick={() =>
-                                handleRoutineRecordDetailClick(data.id)
+                                navigate(ROUTES.RECORD.DETAIL.PATH(data.id))
                             }
                         >
                             <PenIcon />
                             세부 기록보기
                         </IconTextBox.IconText>
-                        <IconTextBox.IconText color={color.warning}>
+                        <IconTextBox.IconText
+                            color={color.warning}
+                            onIconTextClick={() => openRoutineDeleteModal()}
+                        >
                             <RunIcon />
                             루틴 삭제하기
                         </IconTextBox.IconText>
                     </IconTextBox>
                 </Accordion.Body>
-                <Accordion.DeleteButton opacity={opacity} />
+                <Accordion.DeleteButton
+                    opacity={opacity}
+                    onDeleteButtonClick={() => openRoutineDeleteModal()}
+                />
             </Accordion.Motion>
+            <RoutineDeleteModal
+                isOpen={isRoutineDeleteModalOpen}
+                onBackdropClick={() => closeRoutineDeleteModal()}
+                onCancelButtonClick={() => closeRoutineDeleteModal()}
+                onConfirmButtonClick={() => {
+                    closeRoutineDeleteModal();
+                    // TODO: API 연동
+                }}
+            />
         </Accordion>
     );
 };
 
 export default RoutineRecordAccordion;
+
+type RoutineDeleteModalProps = {
+    isOpen: boolean;
+    onBackdropClick: () => void;
+    onCancelButtonClick: () => void;
+    onConfirmButtonClick: () => void;
+};
+
+const RoutineDeleteModal = ({
+    isOpen,
+    onBackdropClick,
+    onCancelButtonClick,
+    onConfirmButtonClick,
+}: RoutineDeleteModalProps) => {
+    return (
+        <Modal>
+            <Modal.Backdrop isOpen={isOpen} onBackdropClick={onBackdropClick} />
+            <Modal.Content isOpen={isOpen}>
+                <Confirm>
+                    <Confirm.ContentBox>
+                        <Confirm.IconBox>
+                            <TrashIcon />
+                        </Confirm.IconBox>
+                        <Confirm.Title>기록 삭제</Confirm.Title>
+                        <Confirm.Description>
+                            운동 기록을 삭제하면 복구할 수 없습니다. <br />
+                            해당 기록을 삭제하시겠습니까?
+                        </Confirm.Description>
+                    </Confirm.ContentBox>
+                    <Confirm.ButtonBox
+                        cancelLabel="취소"
+                        confirmLabel="삭제하기"
+                        onCancelButtonClick={onCancelButtonClick}
+                        onConfirmButtonClick={onConfirmButtonClick}
+                    />
+                </Confirm>
+            </Modal.Content>
+        </Modal>
+    );
+};
