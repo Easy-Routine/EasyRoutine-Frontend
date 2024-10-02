2ffbd016843d160c73dec4679172b8f52a216855
dhkim <dhkim@i2l.co.kr>
2024-10-02 09:52:07 +0900

    style: PageTemplate 컴포넌트 - margin 속성 padding 속성으로 교체

--- src/components/box/PageTemplate/PageTemplate.tsx
+++ src/components/box/PageTemplate/PageTemplate.tsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-    margin-left: 20px;
-    margin-right: 20px;
-    margin-top: 20px;
-    margin-bottom: 85px;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
+    padding-bottom: 85px;
 `;
 
 const PageTemplate = () => {
