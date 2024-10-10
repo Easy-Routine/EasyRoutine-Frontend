246429a1c48627b5713ffdee622979bcb8f08fee
dhkim <dhkim@i2l.co.kr>
2024-10-11 08:45:45 +0900

    feat: RoutineConfigUpdateColorTabBottomBar 컴포넌트 - useUpdateRoutineConfigColorMutation 훅 추가

--- src/components/business/routine-config/RoutineConfigUpdateColorTabBottomBar.tsx
+++ src/components/business/routine-config/RoutineConfigUpdateColorTabBottomBar.tsx
@@ -1,7 +1,9 @@
 import BottomBar from "components/box/BottomBar/BottomBar";
 import ColorTab from "components/box/BottomBar/ColorTab";
 import useTab from "hooks/client/useTab";
+import useUpdateRoutineConfigColorMutation from "hooks/server/useUpdateRoutineConfigColorMutation";
 import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 type RoutineConfigColorTabBottomBarProps = {
     defaultValue: string;
@@ -10,6 +12,8 @@ type RoutineConfigColorTabBottomBarProps = {
 const RoutineConfigColorTabBottomBar = ({
     defaultValue,
 }: RoutineConfigColorTabBottomBarProps) => {
+    const { routineConfigId } = useParams();
+
     const { selectedValue, setSelectedValue, handleTabClick } =
         useTab(defaultValue);
 
@@ -17,8 +21,15 @@ const RoutineConfigColorTabBottomBar = ({
         setSelectedValue(defaultValue);
     }, [defaultValue, setSelectedValue]);
 
-    const handleColorTabClick = (value: string) => {
-        //TODO: routine-config 칼라 수정 API 연결
+    const { mutateAsync: updateRoutineConfigColor } =
+        useUpdateRoutineConfigColorMutation();
+
+    const handleColorTabClick = async (
+        routineConfigId: string,
+        value: string
+    ) => {
+        await updateRoutineConfigColor({ routineConfigId, value });
+
         handleTabClick(value);
     };
 
@@ -27,31 +38,41 @@ const RoutineConfigColorTabBottomBar = ({
             <ColorTab>
                 <ColorTab.Color
                     selectedValue={selectedValue}
-                    onTabClick={handleColorTabClick}
+                    onTabClick={(value) =>
+                        handleColorTabClick(routineConfigId as string, value)
+                    }
                     value="#855CF8"
                     backgroundColor="#855CF8"
                 />
                 <ColorTab.Color
                     selectedValue={selectedValue}
-                    onTabClick={handleColorTabClick}
+                    onTabClick={(value) =>
+                        handleColorTabClick(routineConfigId as string, value)
+                    }
                     value="#F26B2C"
                     backgroundColor="#F26B2C"
                 />
                 <ColorTab.Color
                     selectedValue={selectedValue}
-                    onTabClick={handleColorTabClick}
+                    onTabClick={(value) =>
+                        handleColorTabClick(routineConfigId as string, value)
+                    }
                     value="#2DAF2D"
                     backgroundColor="#2DAF2D"
                 />
                 <ColorTab.Color
                     selectedValue={selectedValue}
-                    onTabClick={handleColorTabClick}
+                    onTabClick={(value) =>
+                        handleColorTabClick(routineConfigId as string, value)
+                    }
                     value="#455A64"
                     backgroundColor="#455A64"
                 />
                 <ColorTab.Color
                     selectedValue={selectedValue}
-                    onTabClick={handleColorTabClick}
+                    onTabClick={(value) =>
+                        handleColorTabClick(routineConfigId as string, value)
+                    }
                     value="#DD408F"
                     backgroundColor="#DD408F"
                 />
