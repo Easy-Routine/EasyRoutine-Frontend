0a4235f647187396bf7496ed1ea8be78c7e7d705
dhkim <dhkim@i2l.co.kr>
2024-10-11 16:59:50 +0900

    feat: useUpdateWorkoutLibraryOneMutation 훅 생성

---
+++ src/hooks/server/useUpdateWorkoutLibraryOneMutation.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import queryKey from "constants/queryKeys";
+import { WorkoutLibrary } from "db";
+import { updateWorkoutLibraryOne } from "services/workout-library";
+
+const useUpdateWorkoutLibraryOneMutation = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: ({
+            workoutLibraryId,
+            updatedData,
+        }: {
+            workoutLibraryId: string;
+            updatedData: Partial<WorkoutLibrary>;
+        }) => updateWorkoutLibraryOne(workoutLibraryId, updatedData),
+        onSettled: () => {
+            queryClient.invalidateQueries({
+                queryKey: [queryKey.getWorkoutLibraryAll],
+            });
+        },
+    });
+};
+
+export default useUpdateWorkoutLibraryOneMutation;
