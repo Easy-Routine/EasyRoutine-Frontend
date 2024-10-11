607f1ec7464307daf160a05c4d36f52d7de62659
dhkim <dhkim@i2l.co.kr>
2024-10-11 13:04:00 +0900

    feat: queryKey 객체 상수 - getWorkoutLibraryOne 속성 추가

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -2,6 +2,7 @@ const queryKey = {
     getRoutineConfigAll: "getRoutineConfigAll",
     getRoutineConfigOne: "getRoutineConfigOne",
     getWorkoutLibraryAll: "getWorkoutLibraryAll",
+    getWorkoutLibraryOne: "getWorkoutLibraryOne",
 } as const;
 
 export default queryKey;
