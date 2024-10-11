4d56a5426586e82ef43dacf1ce6e4748e9526be9
dhkim <dhkim@i2l.co.kr>
2024-10-11 09:48:47 +0900

    feat: TitleText 컴포넌트 - ContentEditable 인자 추가

--- src/components/content/TitleText/TitleText.tsx
+++ src/components/content/TitleText/TitleText.tsx
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
-const TitleText = styled.div`
+const Container = styled.div`
     font-size: 14px;
     font-weight: ${({ theme }) => theme.fontWeight.semibold};
-
     padding: 8px 16px;
     display: flex;
     align-items: center;
+    border: 1px solid transparent; /* 선택적: 편집 가능 영역에 테두리 추가 */
+    outline: none; /* 포커스 시 outline 제거 */
+
+    &:focus {
+        outline: none; /* 포커스 시 outline 제거 */
+    }
 `;
+
+type TitleTextProps = {
+    children: React.ReactNode;
+    contentEditable?: boolean /* true로 설정시 contenteditable 속성을 true로 */;
+};
+
+const TitleText = ({ children, contentEditable = false }: TitleTextProps) => {
+    return <Container contentEditable={contentEditable}>{children}</Container>;
+};
 
 export default TitleText;
