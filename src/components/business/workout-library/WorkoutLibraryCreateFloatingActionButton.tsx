3f4bb902d14ce375150a10ea6efa9a102245cf0d
dhkim <dhkim@i2l.co.kr>
2024-10-13 11:50:29 +0900

    feat: WorkoutLibraryCreateFloatingActionButton 컴포넌트 - useCreateWorkoutLibraryOneMutation 훅 추가

--- src/components/business/workout-library/WorkoutLibraryCreateFloatingActionButton.tsx
+++ src/components/business/workout-library/WorkoutLibraryCreateFloatingActionButton.tsx
@@ -1,5 +1,8 @@
 import FloatingActionButton from "components/content/FloatingActionButton/FloatingActionButton";
+import useCreateWorkoutLibraryOneMutation from "hooks/server/useCreateWorkoutLibraryOneMutation";
 import useToast from "hooks/useToast";
+import { Category } from "type/Category";
+import { Type } from "type/Type";
 
 type WorkoutLibraryCreateFloatingActionButtonProps = {
     onButtonClick: (workoutLibraryId: string) => void;
@@ -9,11 +12,23 @@ const WorkoutLibraryCreateFloatingActionButton = ({
     onButtonClick,
 }: WorkoutLibraryCreateFloatingActionButtonProps) => {
     const { showToast } = useToast();
+    const { mutateAsync: createWorkoutLibraryOneMutate } =
+        useCreateWorkoutLibraryOneMutation();
 
-    const handleButtonClick = () => {
+    const handleButtonClick = async () => {
         // TODO: 워크아웃 라이브러리 생성 API 연결
+        const newWorkoutLibraryOne = await createWorkoutLibraryOneMutate({
+            name: "새 종목",
+            image: "",
+            category: Category.CHEST,
+            type: [Type.REP, Type.WEIGHT],
+            userId: "doggopawer",
+        });
         showToast("운동 종목이 추가되었습니다.");
-        onButtonClick("2");
+
+        if (newWorkoutLibraryOne) {
+            onButtonClick(newWorkoutLibraryOne.id);
+        }
     };
 
     return <FloatingActionButton onButtonClick={handleButtonClick} />;
