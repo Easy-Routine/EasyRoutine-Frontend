575f96f96b3f28659c5da078b7c4a346a0b23b06
dhkim <dhkim@i2l.co.kr>
2024-10-13 20:02:47 +0900

    feat: useGetWorkoutLibraryAllQuery 훅 - name 인자 추가

--- src/hooks/server/useGetWorkoutLibraryAllQuery.ts
+++ src/hooks/server/useGetWorkoutLibraryAllQuery.ts
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import queryKey from "constants/queryKeys";
 import { getWorkoutLibraryAll } from "services/workout-library";
 
-const useGetWorkoutLibraryAllQuery = (category?: string) => {
+const useGetWorkoutLibraryAllQuery = (name?: string, category?: string) => {
     return useQuery({
-        queryKey: [queryKey.getWorkoutLibraryAll, category],
+        queryKey: [queryKey.getWorkoutLibraryAll, category, name],
         queryFn: async () => {
-            const data = await getWorkoutLibraryAll(category);
+            const data = await getWorkoutLibraryAll(name, category);
             return data;
         },
     });
