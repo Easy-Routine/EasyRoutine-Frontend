5727da58eaa46b4629327c6c9d077ec2dab5c34a
dhkim <dhkim@i2l.co.kr>
2024-10-14 21:58:25 +0900

    feat: useGetRoutineRecordAllMonthlyQuery 훅 생성

---
+++ src/hooks/server/useGetRoutineRecordAllMonthlyQuery.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import queryKey from "constants/queryKeys";
+import moment from "moment";
+import { getRoutineRecordAllMonthly } from "services/routine-record";
+
+const useGetRoutineRecordAllMonthlyQuery = (date: Date) => {
+    return useQuery({
+        queryKey: [
+            queryKey.getRoutineRecordAllMonthly,
+            moment(date).startOf("month"),
+        ],
+        queryFn: async () => {
+            const data = await getRoutineRecordAllMonthly({ date });
+            return data;
+        },
+    });
+};
+
+export default useGetRoutineRecordAllMonthlyQuery;
