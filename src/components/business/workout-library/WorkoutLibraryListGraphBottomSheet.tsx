f80adec20e35d8c387a0caae6c743a9cda92760c
dhkim <dhkim@i2l.co.kr>
2024-10-15 23:03:03 +0900

    feat: WorkoutLibraryListGraphBottomSheet 컴포넌트 - useGetWorkoutLibraryAllQuery 훅 추가

--- src/components/business/workout-library/WorkoutLibraryListGraphBottomSheet.tsx
+++ src/components/business/workout-library/WorkoutLibraryListGraphBottomSheet.tsx
@@ -5,14 +5,18 @@ import useInput from "hooks/client/useInput";
 import ChipTab from "components/content/ChipTab/ChipTab";
 import useTab from "hooks/client/useTab";
 import SmallCardList from "components/content/SmallCard/SmallCardList";
-import { WorkoutLibrary } from "types/workout-library";
 import SmallCard from "components/content/SmallCard/SmallCard";
 import SeatedRowImage from "assets/image/seated-row.png";
+import useGetWorkoutLibraryAllQuery from "hooks/server/useGetWorkoutLibraryAllQuery";
+import { WorkoutLibrary } from "db";
+import { Category } from "type/Category";
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: 20px;
+    width: 100%;
+    overflow: auto;
 `;
 
 type WorkoutLibraryListGraphBottomSheetProps = {
@@ -26,44 +30,18 @@ const WorkoutLibraryListGraphBottomSheet = ({
     onBackdropClick,
     onSmallCardClick,
 }: WorkoutLibraryListGraphBottomSheetProps) => {
-    const data: WorkoutLibrary[] = [
-        {
-            id: "1",
-            name: "벤치프레스",
-            workoutImage: SeatedRowImage,
-            workoutPart: "가슴",
-            type: ["weight", "rep"],
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            userId: 1,
-        },
-        {
-            id: "2",
-            name: "데드리프트",
-            workoutImage: SeatedRowImage,
-            workoutPart: "등",
-            type: ["weight", "rep"],
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            userId: 1,
-        },
-        {
-            id: "3",
-            name: "스쿼트",
-            workoutImage: SeatedRowImage,
-            workoutPart: "하체",
-            type: ["weight", "rep"],
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            userId: 1,
-        },
-    ];
-
     const { value, handleInputChange, handleInputClear } = useInput();
     const {
         selectedValue: selectedChipTabValue,
         handleTabClick: handleChipTabClick,
-    } = useTab("가슴");
+    } = useTab(Category.ALL);
+
+    const { data: workoutLibraryAllData } = useGetWorkoutLibraryAllQuery(
+        value,
+        selectedChipTabValue
+    );
+
+    const workoutLibraryAll = workoutLibraryAllData ?? [];
 
     return (
         <Modal>
@@ -77,42 +55,49 @@ const WorkoutLibraryListGraphBottomSheet = ({
                     />
                     <ChipTab>
                         <ChipTab.Chip
-                            value="가슴"
+                            value={Category.ALL}
+                            selectedValue={selectedChipTabValue}
+                            onTabClick={handleChipTabClick}
+                        >
+                            전체
+                        </ChipTab.Chip>
+                        <ChipTab.Chip
+                            value={Category.CHEST}
                             selectedValue={selectedChipTabValue}
                             onTabClick={handleChipTabClick}
                         >
                             가슴
                         </ChipTab.Chip>
                         <ChipTab.Chip
-                            value="등"
+                            value={Category.BACK}
                             selectedValue={selectedChipTabValue}
                             onTabClick={handleChipTabClick}
                         >
                             등
                         </ChipTab.Chip>
                         <ChipTab.Chip
-                            value="어깨"
+                            value={Category.SHOULDER}
                             selectedValue={selectedChipTabValue}
                             onTabClick={handleChipTabClick}
                         >
                             어깨
                         </ChipTab.Chip>
                         <ChipTab.Chip
-                            value="하체"
+                            value={Category.LEG}
                             selectedValue={selectedChipTabValue}
                             onTabClick={handleChipTabClick}
                         >
                             하체
                         </ChipTab.Chip>
                         <ChipTab.Chip
-                            value="팔"
+                            value={Category.ARM}
                             selectedValue={selectedChipTabValue}
                             onTabClick={handleChipTabClick}
                         >
                             팔
                         </ChipTab.Chip>
                         <ChipTab.Chip
-                            value="기타"
+                            value={Category.ETC}
                             selectedValue={selectedChipTabValue}
                             onTabClick={handleChipTabClick}
                         >
@@ -121,7 +106,7 @@ const WorkoutLibraryListGraphBottomSheet = ({
                     </ChipTab>
 
                     <SmallCardList<WorkoutLibrary>
-                        data={data}
+                        data={workoutLibraryAll}
                         render={({ id, name }) => (
                             <SmallCard
                                 onCardClick={() => onSmallCardClick(id)}
