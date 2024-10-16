a29d06a8b35878691b66ef300693a70ceb20efd9
dhkim <dhkim@i2l.co.kr>
2024-10-16 21:33:01 +0900

    feat: ImageInput 컴포넌트 생성

---
+++ src/components/content/ImageInput/ImageInput.tsx
@@ -0,0 +1,38 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+import styled from "styled-components";
+import { ReactComponent as PlaceholderIcon } from "assets/image/img-placeholder.svg";
+
+const Container = styled.label`
+    width: 150px;
+    height: 150px;
+    padding: 10px;
+    border-radius: ${({ theme }) => theme.borderRadius.xl};
+    border: 1px solid ${({ theme }) => theme.color.gray.normal};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+const Input = styled.input`
+    display: none;
+`;
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+`;
+
+type ImageInputProps = {
+    value: string;
+    onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const ImageInput = ({ value, onInputChange }: ImageInputProps) => {
+    return (
+        <Container>
+            <Input type="file" accept="image/*" onChange={onInputChange} />
+            {value ? <Image src={value} /> : <PlaceholderIcon />}
+        </Container>
+    );
+};
+
+export default ImageInput;
