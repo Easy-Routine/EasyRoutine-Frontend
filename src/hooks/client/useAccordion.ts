4de49dc06d5506595b5033247839e9fd1b84dabd
dhkim <dhkim@i2l.co.kr>
2024-10-16 21:32:44 +0900

    fix: useAccordion 훅 - handleDragEnd 핸들러 수정, 민감도 설정 추가

--- src/hooks/client/useAccordion.ts
+++ src/hooks/client/useAccordion.ts
@@ -13,14 +13,20 @@ const useAccordion = () => {
     // }, [isCurrentAccordion]);
 
     const handleDragEnd = (_: any, info: { offset: { x: number } }) => {
-        if (info.offset.x > 0) {
+        const dragThreshold = 150; // 드래그 민감도 설정 (20픽셀)
+
+        if (info.offset.x > dragThreshold) {
             // 오른쪽으로 드래그
             setX(0); // 원래 위치로 돌아가기
             setOpacity(0);
-        } else {
+        } else if (info.offset.x < -dragThreshold) {
             // 왼쪽으로 드래그
             setX(-65); // 왼쪽으로 이동하여 삭제 버튼 보이기
             setOpacity(1);
+        } else {
+            // 드래그가 민감도 이하일 경우 원래 위치로 돌아가기
+            setX(0);
+            setOpacity(0);
         }
     };
 
