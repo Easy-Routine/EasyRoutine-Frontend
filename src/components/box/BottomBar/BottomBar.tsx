8a03f77db281e2ee9834b655b84f72c33bd4fcce
dhkim <dhkim@i2l.co.kr>
2024-11-01 22:30:10 +0900

    feat: BottomBar - MaxWidthWrapper의 크기와 위치를 참조해 스타일 재설정 기능 추가

--- src/components/box/BottomBar/BottomBar.tsx
+++ src/components/box/BottomBar/BottomBar.tsx
@@ -1,9 +1,10 @@
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
     position: fixed;
-    left: 0;
     bottom: 0;
+    left: 0;
     width: 100%;
     height: 65px;
     padding: 10px 20px;
@@ -23,7 +24,28 @@ type BottomBarProps = {
 };
 
 const BottomBar = ({ children }: BottomBarProps) => {
-    return <Container>{children}</Container>;
+    const bottomBarRef = useRef<HTMLDivElement>(null);
+
+    const updateBottomBarPosition = () => {
+        const wrapElement = document.getElementById("wrap");
+        if (wrapElement && bottomBarRef.current) {
+            const wrapRect = wrapElement.getBoundingClientRect();
+            bottomBarRef.current.style.width = `${wrapElement.clientWidth}px`;
+            bottomBarRef.current.style.left = `${wrapRect.left}px`; // wrap의 left 위치에 맞춤
+            bottomBarRef.current.style.bottom = `0px`; // window의 하단에 맞춤
+        }
+    };
+
+    useEffect(() => {
+        updateBottomBarPosition(); // 초기 위치 설정
+
+        window.addEventListener("resize", updateBottomBarPosition); // 리사이즈 이벤트 리스너 추가
+        return () => {
+            window.removeEventListener("resize", updateBottomBarPosition); // 클린업 함수로 리스너 제거
+        };
+    }, []);
+
+    return <Container ref={bottomBarRef}>{children}</Container>;
 };
 
 export default BottomBar;
