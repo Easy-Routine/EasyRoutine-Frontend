def506a20722844608763ddd19c8a1664cf56778
dhkim <dhkim@i2l.co.kr>
2024-11-01 22:29:06 +0900

    feat: App - MaxWidthWrapper 생성 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import AppRouter from "pages/AppRouter";
+import styled from "styled-components";
+
+const MaxWidthWrapper = styled.div`
+    max-width: 640px;
+    margin: 0 auto;
+    position: relative;
+    width: 100%;
+`;
 
 const App = () => {
-    return <AppRouter />;
+    return (
+        <MaxWidthWrapper id="wrap">
+            <AppRouter />
+        </MaxWidthWrapper>
+    );
 };
 
 export default App;
