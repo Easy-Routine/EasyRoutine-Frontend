f9fd659536286bc7693e24211cb45d83afaa444a
dhkim <dhkim@i2l.co.kr>
2024-11-04 13:07:22 +0900

    feat: MyPage/handleSyncModeToggleClick - syncMode를 토글 하는 기능

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,20 +2,17 @@ import NavigationBottomBar from "components/business/NavigationBottomBar";
 import Logo from "components/content/Logo/Logo";
 import UnderlineBox from "components/content/UnderlineBox/UnderlineBox";
 import ROUTES from "constants/routes";
-import styled, { useTheme } from "styled-components";
+import styled from "styled-components";
 import { ReactComponent as MoonIcon } from "assets/image/moon.svg";
 import { ReactComponent as HumanIcon } from "assets/image/human2.svg";
 import Toggle from "components/content/Toggle/Toggle";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext, ThemeContextType } from "context/ThemeContext";
 import { db } from "db";
-import { APIContext, APIContextType } from "context/APIProvider";
 import useToast from "hooks/useToast";
-import { AxiosError } from "axios";
-import { useRecoilValue } from "recoil";
-import { userContextStore } from "store/userContextStore";
 import convertDateStringsToDateObjects from "utils/convertDateStringsToDateObjects";
 import api from "utils/axios";
+import syncData from "utils/syncData";
 
 const Container = styled.div`
     display: flex;
@@ -31,47 +28,25 @@ const MyPage = () => {
     ) as ThemeContextType;
     const { showToast } = useToast();
 
-    const handleToggleButtonClick = () => {
+    const [syncMode, setSyncMode] = useState(
+        localStorage.getItem("syncMode") &&
+            localStorage.getItem("syncMode") === "on"
+            ? true
+            : false
+    );
+
+    const handleDarkModeToggleClick = () => {
         setThemeMode(themeMode === "dark" ? "light" : "dark");
     };
 
-    const userId = localStorage.getItem("userId");
+    const handleSyncModeToggleClick = () => {
+        localStorage.setItem("syncMode", syncMode ? "off" : "on");
+        setSyncMode(!syncMode);
+    };
 
-    const handleTest = async () => {
-        const routineConfigs = await db.routineConfigs
-            .where("userId") // userId 필드에 대해 조건 설정
-            .equals(userId as string) // userId와 일치하는 데이터만 가져오기
-            .toArray();
-
-        const routineRecords = await db.routineRecords
-            .where("userId") // userId 필드에 대해 조건 설정
-            .equals(userId as string) // userId와 일치하는 데이터만 가져오기
-            .toArray();
-
-        const workoutLibraries = await db.workoutLibraries
-            .where("userId") // userId 필드에 대해 조건 설정
-            .equals(userId as string) // userId와 일치하는 데이터만 가져오기
-            .toArray();
-
-        const data = { routineConfigs, routineRecords, workoutLibraries };
+    const handleSyncButtonClick = async () => {
         try {
-            const response = await api.post("/sync", data);
-
-            const { routineConfigs, routineRecords, workoutLibraries } =
-                response.data;
-
-            console.log(response);
-
-            // IndexedDB에 저장
-            await db.routineConfigs.bulkPut(
-                convertDateStringsToDateObjects(routineConfigs)
-            );
-            await db.routineRecords.bulkPut(
-                convertDateStringsToDateObjects(routineRecords)
-            );
-            await db.workoutLibraries.bulkPut(
-                convertDateStringsToDateObjects(workoutLibraries)
-            );
+            await syncData();
         } catch (e) {
             showToast("로그인이 만료되었습니다.", "error");
         }
@@ -96,7 +71,7 @@ const MyPage = () => {
                     </UnderlineBox.TitleWrapper>
                     <Toggle
                         defaultValue={themeMode === "dark"}
-                        onToggleChange={handleToggleButtonClick}
+                        onToggleChange={handleDarkModeToggleClick}
                     />
                 </UnderlineBox>
                 <UnderlineBox>
@@ -110,14 +85,17 @@ const MyPage = () => {
                         <MoonIcon />
                         동기화
                     </UnderlineBox.TitleWrapper>
-                    <button onClick={handleTest}>동기화테스트</button>
+                    <button onClick={handleSyncButtonClick}>동기화</button>
                 </UnderlineBox>
                 <UnderlineBox>
                     <UnderlineBox.TitleWrapper>
                         <MoonIcon />
                         자동 동기화 설정
                     </UnderlineBox.TitleWrapper>
-                    <button onClick={handleTest}>동기화테스트</button>
+                    <Toggle
+                        defaultValue={syncMode}
+                        onToggleChange={handleSyncModeToggleClick}
+                    />
                 </UnderlineBox>
                 <UnderlineBox>
                     <UnderlineBox.TitleWrapper>
