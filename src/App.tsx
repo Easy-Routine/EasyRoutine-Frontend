01344a6a189be4ea77e5a2415f006b08a719686f
dhkim <dhkim@i2l.co.kr>
2024-11-04 13:12:18 +0900

    feat: App - syncDataPeriodically, 7일 간격으로 조건에 맞춰 데이터를 동기화 - handleAppMounted, 앱이 최초 마운트시, syncDataPeriodically 호출

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import moment from "moment";
 import AppRouter from "pages/AppRouter";
+import { useEffect } from "react";
 import styled from "styled-components";
+import syncData from "utils/syncData";
 
 const MaxWidthWrapper = styled.div`
     max-width: 640px;
@@ -9,6 +12,32 @@ const MaxWidthWrapper = styled.div`
 `;
 
 const App = () => {
+    const syncDataPeriodically = async () => {
+        const lastUpdateDate = localStorage.getItem("lastUpdateDate");
+        const syncMode = localStorage.getItem("syncMode");
+
+        if (syncMode === "on") {
+            if (!lastUpdateDate) {
+                await syncData();
+                localStorage.setItem("lastUpdateDate", moment().toISOString());
+                return;
+            }
+            const hasUpdateDatePassed = moment(lastUpdateDate).isBefore(
+                moment().subtract(7, "days")
+            );
+            if (hasUpdateDatePassed) {
+                await syncData();
+                localStorage.setItem("lastUpdateDate", moment().toISOString());
+            }
+        }
+    };
+    // handleAppMounted
+    useEffect(() => {
+        (async () => {
+            await syncDataPeriodically();
+        })();
+    }, []);
+
     return (
         <MaxWidthWrapper id="wrap">
             <AppRouter />
