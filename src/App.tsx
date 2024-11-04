e8341e42941870268c1d9fff2f923d348d6145d6
dhkim <dhkim@i2l.co.kr>
2024-11-04 23:03:31 +0900

    feat: App - 앱 최초 마운트 동기화시 DataSyncModal 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import DataSyncModal from "components/business/DataSyncModal";
+import useModal from "hooks/client/useModal";
+import useToast from "hooks/useToast";
 import moment from "moment";
 import AppRouter from "pages/AppRouter";
 import { useEffect } from "react";
@@ -13,32 +16,65 @@ const MaxWidthWrapper = styled.div`
 `;
 
 const App = () => {
-    const syncDataPeriodically = async () => {
-        const lastUpdateDate = localStorage.getItem("lastUpdateDate");
-        const syncMode = localStorage.getItem("syncMode");
+    const {
+        isOpen: isDataSyncModalOpen,
+        handleOpenModal: openDataSyncModal,
+        handleCloseModal: closeDataSyncModal,
+    } = useModal();
+    const { showToast } = useToast();
 
-        if (syncMode === "on") {
-            if (!lastUpdateDate) {
-                await syncData();
-                localStorage.setItem("lastUpdateDate", moment().toISOString());
-                return;
+    const syncDataPeriodically = async () => {
+        try {
+            const lastUpdateDate = localStorage.getItem("lastUpdateDate");
+            const syncMode = localStorage.getItem("syncMode");
+
+            if (syncMode === "on") {
+                if (!lastUpdateDate) {
+                    await syncData();
+                    localStorage.setItem(
+                        "lastUpdateDate",
+                        moment().toISOString()
+                    );
+                    showToast("데이터를 동기화했습니다.", "success");
+                    return;
+                }
+                const hasUpdateDatePassed = moment(lastUpdateDate).isBefore(
+                    moment().subtract(7, "days")
+                );
+                if (hasUpdateDatePassed) {
+                    await syncData();
+                    localStorage.setItem(
+                        "lastUpdateDate",
+                        moment().toISOString()
+                    );
+                    showToast("데이터를 동기화했습니다.", "success");
+                }
             }
-            const hasUpdateDatePassed = moment(lastUpdateDate).isBefore(
-                moment().subtract(7, "days")
-            );
-            if (hasUpdateDatePassed) {
-                await syncData();
-                localStorage.setItem("lastUpdateDate", moment().toISOString());
-            }
+        } catch (e) {
+            showToast("데이터 동기화에 실패 했습니다.", "error");
         }
     };
 
     // handleAppMounted
     useEffect(() => {
         (async () => {
-            const isAccessTokenFresh = await checkAccessToken();
-            if (isAccessTokenFresh) {
-                await syncDataPeriodically();
+            let modalOpenTimeout;
+            try {
+                const isAccessTokenFresh = await checkAccessToken();
+
+                if (isAccessTokenFresh) {
+                    const syncPromise = syncDataPeriodically();
+
+                    modalOpenTimeout = setTimeout(() => {
+                        openDataSyncModal();
+                    }, 1000);
+
+                    await syncPromise;
+                }
+            } catch (e) {
+            } finally {
+                clearTimeout(modalOpenTimeout); // 모달 열기 타이머를 클리어
+                closeDataSyncModal(); // 모달 닫기
             }
         })();
     }, []);
@@ -46,6 +82,7 @@ const App = () => {
     return (
         <MaxWidthWrapper id="wrap">
             <AppRouter />
+            <DataSyncModal isOpen={isDataSyncModalOpen} />
         </MaxWidthWrapper>
     );
 };
