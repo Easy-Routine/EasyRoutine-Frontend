b4c1e3c0ab04f2e37cad20274a8b1c9332ccb654
dhkim <dhkim@i2l.co.kr>
2024-11-21 09:57:26 +0900

    fix: 아이폰, 스크롤 생김 현상 해결 - useModal, DOM 코드 변경

--- src/hooks/client/useModal.ts
+++ src/hooks/client/useModal.ts
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useModal = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleOpenModal = () => {
         setIsOpen(true);
-        document.body.style.overflowY = "hidden";
     };
+
     const handleCloseModal = () => {
         setIsOpen(false);
-        document.body.style.overflowY = "scroll";
     };
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = "hidden"; // 스크롤 숨기기
+        } else {
+            document.body.style.overflow = ""; // 원래 상태로 복원
+        }
+
+        // Clean up function to reset overflow when component unmounts
+        return () => {
+            document.body.style.overflow = "";
+        };
+    }, [isOpen]);
 
     return { isOpen, handleOpenModal, handleCloseModal };
 };
