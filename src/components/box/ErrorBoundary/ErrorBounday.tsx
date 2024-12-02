18068ad23060bf4f2e600cc3f8390f28e9a67562
dhkim <dhkim@i2l.co.kr>
2024-12-02 13:06:00 +0900

    feat: 에러바운더리에 에러페이지 적용

--- src/components/box/ErrorBoundary/ErrorBounday.tsx
+++ src/components/box/ErrorBoundary/ErrorBounday.tsx
@@ -1,11 +1,15 @@
+import ErrorPage from "pages/ErrorPage";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface ErrorBoundaryProps {
     children: React.ReactNode; // 자식 요소 타입 정의
+    navigate: (path: string, options?: any) => void; // navigate 함수 타입 정의
 }
 
 interface ErrorBoundaryState {
     hasError: boolean; // 에러 발생 여부를 나타내는 상태
+    error?: Error; // 에러 객체를 저장할 수 있는 상태
 }
 
 class ErrorBoundary extends React.Component<
@@ -14,28 +18,35 @@ class ErrorBoundary extends React.Component<
 > {
     constructor(props: ErrorBoundaryProps) {
         super(props);
-        this.state = { hasError: false };
+        this.state = { hasError: false, error: undefined };
     }
 
     static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-        console.log();
-        return { hasError: true };
+        return { hasError: true, error }; // 에러 객체를 상태에 저장
     }
 
     componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
         console.error("Error logged:", error, errorInfo);
     }
 
+    resetError = () => {
+        this.setState({ hasError: false, error: undefined });
+        window.location.reload();
+    };
+
     render() {
-        console.log("hasError", this.state.hasError);
         if (this.state.hasError) {
-            return <h1>무언가 잘못되었습니다.</h1>;
+            return <ErrorPage onReset={this.resetError} />;
         } else {
             return this.props.children;
         }
-
-        // 에러가 없으면 자식 요소를 렌더링
     }
 }
 
-export default ErrorBoundary;
+// withRouter를 사용하여 navigate를 props로 전달
+const ErrorBoundaryWithRouter = (props: any) => {
+    const navigate = useNavigate();
+    return <ErrorBoundary {...props} navigate={navigate} />;
+};
+
+export default ErrorBoundaryWithRouter;
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { ReactComponent as EmptyImage } from "assets/image/empty.svg";
+import Button from "components/content/Button/Button";
+import Logo from "components/content/Logo/Logo";
+import ROUTES from "constants/routes";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: ${({ theme }) => theme.color.background.page};
+    z-index: 500;
+`;
+const ErrorContent = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+    width: 80%;
+`;
+
+const Title = styled.div`
+    font-size: ${({ theme }) => theme.fontSize.xxl};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    color: ${({ theme }) => theme.color.text.black};
+`;
+const Description = styled.div`
+    font-size: ${({ theme }) => theme.fontSize.lg};
+    color: ${({ theme }) => theme.color.gray.light};
+`;
+
+const ErrorPage = ({ onReset }: { onReset: () => void }) => {
+    return (
+        <Container>
+            <ErrorContent>
+                <Logo type="large" />
+                <Title>에러가 발생했습니다.</Title>
+                <Description>서비스 이용에 불편을 드려 죄송합니다.</Description>
+                <Button onClick={onReset}>돌아가기</Button>
+            </ErrorContent>
+        </Container>
+    );
+};
+
+export default ErrorPage;
