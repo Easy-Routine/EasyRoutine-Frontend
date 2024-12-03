1dac01ff04114f668c2b329c04fd3500029e6b3a
dhkim <dhkim@i2l.co.kr>
2024-12-03 10:16:10 +0900

    feat: 폰트 교체 - ttf => woff2 로 교체

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,9 +1,36 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import NotoSansKoreanBold from "assets/fonts/NotoSansKR-Bold.ttf";
+import NotoSansKoreanSemiBold from "assets/fonts/NotoSansKR-SemiBold.ttf";
+import NotoSansKoreanRegular from "assets/fonts/NotoSansKR-Regular.ttf";
 
 export const GlobalStyle = createGlobalStyle`
 	${reset}
+// 	@font-face {
+//     font-family: "Noto Sans Korean";
+//     src: url(${NotoSansKoreanRegular}) format("woff2");
+//     font-weight: 400;
+//     font-style: normal;
+//     // font-display: swap;
+// }
 
+// @font-face {
+//     font-family: "Noto Sans Korean";
+//     src: url(${NotoSansKoreanSemiBold}) format("woff2");
+//     font-weight: 600;
+//     font-style: normal;
+//     // font-display: swap;
+// }
+
+// @font-face {
+//     font-family: "Noto Sans Korean";
+//     src: url(${NotoSansKoreanBold}) format("woff2");
+//     font-weight: 700; /* ExtraBold */
+//     font-style: normal;
+//     // font-display: swap;
+// }
+
+	
 	* {
 		overscroll-behavior: none;
 	}
@@ -18,13 +45,16 @@ export const GlobalStyle = createGlobalStyle`
 	body {
 		background: ${(props) => props.theme.color.background.page};
         color: ${(props) => props.theme.color.text.black};
-		font-family: 'Noto Sans Korean', sans-serif;
+		// font-family: 'Noto Sans Korean', sans-serif;
+		font-family: sans-serif;
 	}
 	input {
-	    font-family: 'Noto Sans Korean', sans-serif;
+	    // font-family: 'Noto Sans Korean', sans-serif;
+		font-family: sans-serif;
 	}
 	button {
-		font-family: 'Noto Sans Korean', sans-serif;
+		// font-family: 'Noto Sans Korean', sans-serif;
+		font-family: sans-serif;
 	}
 
 `;
