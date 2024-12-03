7a3c76ff46ab25c6475184e49cedd27f1ac43385
dhkim <dhkim@i2l.co.kr>
2024-12-03 22:32:42 +0900

    feat: 리액트 앱 이젝트

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-import "index.css";
 import App from "App";
 import ThemeProvider from "context/ThemeContext";
 import { GlobalStyle } from "style/GlobalStyle";
--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -6,31 +6,7 @@ import NotoSansKoreanRegular from "assets/fonts/NotoSansKR-Regular.ttf";
 
 export const GlobalStyle = createGlobalStyle`
 	${reset}
-// 	@font-face {
-//     font-family: "Noto Sans Korean";
-//     src: url(${NotoSansKoreanRegular}) format("woff2");
-//     font-weight: 400;
-//     font-style: normal;
-//     // font-display: swap;
-// }
 
-// @font-face {
-//     font-family: "Noto Sans Korean";
-//     src: url(${NotoSansKoreanSemiBold}) format("woff2");
-//     font-weight: 600;
-//     font-style: normal;
-//     // font-display: swap;
-// }
-
-// @font-face {
-//     font-family: "Noto Sans Korean";
-//     src: url(${NotoSansKoreanBold}) format("woff2");
-//     font-weight: 700; /* ExtraBold */
-//     font-style: normal;
-//     // font-display: swap;
-// }
-
-	
 	* {
 		overscroll-behavior: none;
 	}
@@ -45,16 +21,16 @@ export const GlobalStyle = createGlobalStyle`
 	body {
 		background: ${(props) => props.theme.color.background.page};
         color: ${(props) => props.theme.color.text.black};
-		// font-family: 'Noto Sans Korean', sans-serif;
-		font-family: sans-serif;
+		font-family: 'Noto Sans Korean', sans-serif;
+		// font-family: sans-serif;
 	}
 	input {
-	    // font-family: 'Noto Sans Korean', sans-serif;
-		font-family: sans-serif;
+	    font-family: 'Noto Sans Korean', sans-serif;
+		// font-family: sans-serif;
 	}
 	button {
-		// font-family: 'Noto Sans Korean', sans-serif;
-		font-family: sans-serif;
+		font-family: 'Noto Sans Korean', sans-serif;
+		// font-family: sans-serif;
 	}
 
 `;
