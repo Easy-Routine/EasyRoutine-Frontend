8927571c6de478f4b14a099bf17b44df205cda4f
dhkim <dhkim@i2l.co.kr>
2024-12-21 02:16:34 +0900

    fix: 데이터 동기화 로직 PrivateRoute로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import DataSyncModal from "components/business/DataSyncModal";
 import SideBanner from "components/content/SideBanner/SideBanner";
 import SplashScrren from "components/content/SplashScreen/SplashScreen";
-import { db } from "db";
+import {db} from "db";
 import useModal from "hooks/client/useModal";
-import useToast from "hooks/useToast";
-import moment from "moment";
 import AppRouter from "pages/AppRouter";
-import { useEffect, useState } from "react";
-import { checkAccessToken, getBaseWorkout } from "services";
+import {useEffect, useState} from "react";
+import {getBaseWorkout} from "services";
 import styled from "styled-components";
-import syncData from "utils/syncData";
 
 const MaxWidthWrapper = styled.div`
     max-width: 640px;
@@ -19,48 +15,9 @@ const MaxWidthWrapper = styled.div`
 `;
 
 const App = () => {
-    const {
-        isOpen: isDataSyncModalOpen,
-        handleOpenModal: openDataSyncModal,
-        handleCloseModal: closeDataSyncModal,
-    } = useModal();
-    const { showToast } = useToast();
-
     const isSplashScreenShown = !!sessionStorage.getItem("isSplashScreenShown");
-    const { isOpen: isSplashScreenOpen, handleCloseModal: closeSplashScreen } =
+    const {isOpen: isSplashScreenOpen, handleCloseModal: closeSplashScreen} =
         useModal(isSplashScreenShown ? false : true);
-
-    const syncDataPeriodically = async () => {
-        try {
-            const lastUpdateDate = localStorage.getItem("lastUpdateDate");
-            const syncMode = localStorage.getItem("syncMode");
-
-            if (syncMode === "on") {
-                if (!lastUpdateDate) {
-                    await syncData();
-                    localStorage.setItem(
-                        "lastUpdateDate",
-                        moment().toISOString()
-                    );
-                    showToast("데이터를 동기화했습니다.", "success");
-                    return;
-                }
-                const hasUpdateDatePassed = moment(lastUpdateDate).isBefore(
-                    moment().subtract(7, "days")
-                );
-                if (hasUpdateDatePassed) {
-                    await syncData();
-                    localStorage.setItem(
-                        "lastUpdateDate",
-                        moment().toISOString()
-                    );
-                    showToast("데이터를 동기화했습니다.", "success");
-                }
-            }
-        } catch (e) {
-            showToast("데이터 동기화에 실패 했습니다.", "error");
-        }
-    };
 
     const getBaseWorkoutAPI = async () => {
         const hasBaseWorkout = localStorage.getItem("hasBaseWorkout");
@@ -78,9 +35,6 @@ const App = () => {
         let timer: NodeJS.Timer;
 
         (async () => {
-            let modalOpenTimeout;
-
-            // 스크린이 열려있고, 스크린이 열렸던 적이 없으면 => 스크린이 열렸던적이 잇음 => 안닫힘
             if (isSplashScreenOpen) {
                 timer = setTimeout(() => {
                     sessionStorage.setItem("isSplashScreenShown", "true");
@@ -88,22 +42,10 @@ const App = () => {
                 }, 3000);
             } else {
                 try {
-                    const isAccessTokenFresh = await checkAccessToken();
                     await getBaseWorkoutAPI();
-                    if (isAccessTokenFresh) {
-                        const syncPromise = syncDataPeriodically();
-
-                        modalOpenTimeout = setTimeout(() => {
-                            openDataSyncModal();
-                        }, 1000);
-
-                        await syncPromise;
-                    }
                 } catch (e) {
                 } finally {
-                    clearTimeout(modalOpenTimeout); // 모달 열기 타이머를 클리어
                 }
-                closeDataSyncModal(); // 모달 닫기
             }
         })();
 
@@ -114,7 +56,6 @@ const App = () => {
         <MaxWidthWrapper id="wrap">
             {isSplashScreenOpen && <SplashScrren />}
             <AppRouter />
-            <DataSyncModal isOpen={isDataSyncModalOpen} />
             <SideBanner />
         </MaxWidthWrapper>
     );
