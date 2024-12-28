1b35b570c5e57a55b4bb6723c135381bcaab7e11
dhkim <dhkim@i2l.co.kr>
2024-12-28 14:56:06 +0900

    fix: 토스트 UI 지속시간 2초로 수정정

--- src/components/content/Toast/Toast.tsx
+++ src/components/content/Toast/Toast.tsx
@@ -1,48 +1,48 @@
-import { ToastContext, ToastContextType } from "context/ToastContext";
-import { useContext, useEffect } from "react";
-import styled, { useTheme } from "styled-components";
+import {ToastContext, ToastContextType} from "context/ToastContext";
+import {useContext, useEffect} from "react";
+import styled, {useTheme} from "styled-components";
 import CircleBox from "components/box/CircleBox/CircleBox";
-import { ReactComponent as XIcon } from "assets/image/x.svg";
-import { ReactComponent as CheckIcon } from "assets/image/check.svg";
+import {ReactComponent as XIcon} from "assets/image/x.svg";
+import {ReactComponent as CheckIcon} from "assets/image/check.svg";
 
-const Container = styled.div<{ $isOpen: boolean }>`
+const Container = styled.div<{$isOpen: boolean}>`
     position: fixed;
     top: 0;
     left: 50%;
     width: 80%;
-    background-color: ${({ theme }) => theme.color.background.box};
-    border-radius: ${({ theme }) => theme.borderRadius.xl};
+    background-color: ${({theme}) => theme.color.background.box};
+    border-radius: ${({theme}) => theme.borderRadius.xl};
     display: flex;
     align-items: center;
     gap: 10px;
     padding: 10px 40px;
     box-sizing: border-box;
     transition: all 0.5s ease-in-out;
-    transform: ${({ $isOpen }) =>
+    transform: ${({$isOpen}) =>
         $isOpen ? "translate(-50%, 25%)" : "translate(-50%, -100%)"};
-    opacity: ${({ $isOpen }) => ($isOpen ? "1" : "0")};
-    z-index: ${({ theme }) => theme.zIndex.modal};
-    box-shadow: ${({ theme }) => theme.boxShadow};
+    opacity: ${({$isOpen}) => ($isOpen ? "1" : "0")};
+    z-index: ${({theme}) => theme.zIndex.modal};
+    box-shadow: ${({theme}) => theme.boxShadow};
 `;
 
 const Text = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.md};
-    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    font-size: ${({theme}) => theme.fontSize.md};
+    font-weight: ${({theme}) => theme.fontWeight.semibold};
 `;
 
 const Toast = () => {
     // 프로바이더로 가져오기
-    const { isOpen, setIsOpen, type } = useContext(
-        ToastContext
+    const {isOpen, setIsOpen, type} = useContext(
+        ToastContext,
     ) as ToastContextType;
-    const { message } = useContext(ToastContext) as ToastContextType;
-    const { color } = useTheme();
+    const {message} = useContext(ToastContext) as ToastContextType;
+    const {color} = useTheme();
 
     useEffect(() => {
         if (isOpen) {
             setTimeout(() => {
                 setIsOpen(false);
-            }, 3000);
+            }, 2000);
         }
     }, [isOpen, setIsOpen]);
 
