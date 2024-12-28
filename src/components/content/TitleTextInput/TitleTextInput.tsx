db83162184e68e7f7f6c0bf349dffb8d2e44dfb6
dhkim <dhkim@i2l.co.kr>
2024-12-28 15:15:18 +0900

    feat: disabled시 텍스트 색 회색으로 설정정

--- src/components/content/TitleTextInput/TitleTextInput.tsx
+++ src/components/content/TitleTextInput/TitleTextInput.tsx
@@ -1,19 +1,19 @@
-import { ChangeEvent } from "react";
+import {ChangeEvent} from "react";
 import styled from "styled-components";
 
 const Container = styled.input`
     width: 100%;
     font-size: 14px;
-    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    font-weight: ${({theme}) => theme.fontWeight.semibold};
     padding: 8px 16px;
     border: 1px solid transparent; /* 선택적: 편집 가능 영역에 테두리 추가 */
     outline: none; /* 포커스 시 outline 제거 */
     box-sizing: border-box;
     border-radius: inherit;
-    background-color: ${({ theme }) => theme.color.background.box};
-    color: ${({ theme }) => theme.color.text.black};
+    background-color: ${({theme}) => theme.color.background.box};
+    color: ${({theme}) => theme.color.text.black};
     &:disabled {
-        color: inherit;
+        color: ${({theme}) => theme.color.gray.normal};
         background-color: transparent;
     }
 `;
