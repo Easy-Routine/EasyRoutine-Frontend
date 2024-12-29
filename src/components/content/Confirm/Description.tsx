41d7b4046b04a8741fbf01ca5291cce10042deda
dhkim <dhkim@i2l.co.kr>
2024-12-29 11:38:30 +0900

    style: 모달 내용 단어별로 줄바꿈 적용

--- src/components/content/Confirm/Description.tsx
+++ src/components/content/Confirm/Description.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 const Description = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.md};
-    font-weight: ${({ theme }) => theme.fontWeight.regular};
+    font-size: ${({theme}) => theme.fontSize.md};
+    font-weight: ${({theme}) => theme.fontWeight.regular};
     padding: 20px 0;
     text-align: center;
+    line-height: ${({theme}) => theme.fontSize.lg};
+    word-break: keep-all;
 `;
 
 export default Description;
