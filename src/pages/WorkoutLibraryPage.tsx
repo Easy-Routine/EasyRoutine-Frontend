8de6fbfd066011fcb9f49e51b1231e59917525b3
dhkim <dhkim@i2l.co.kr>
2025-01-02 21:40:03 +0900

    feat: 툴팁 임시 적용용

--- src/pages/WorkoutLibraryPage.tsx
+++ src/pages/WorkoutLibraryPage.tsx
@@ -5,8 +5,9 @@ import WorkoutLibraryListView from "components/business/workout-library/WorkoutL
 import CommonLoading from "components/content/CommonLoading/CommonLoading";
 import Logo from "components/content/Logo/Logo";
 import PageHeader from "components/content/PageHeader/PageHeader";
+import ToolTip from "components/content/ToolTip/ToolTip";
 import ROUTES from "constants/routes";
-import { Suspense } from "react";
+import {Suspense} from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -14,12 +15,20 @@ const Container = styled.div`
     flex-direction: column;
     gap: 20px;
 `;
+const Flex = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`;
 
 const WorkoutLibraryPage = () => {
     return (
         <Container>
             <PageHeader>
-                <Logo />
+                <Flex>
+                    <Logo />
+                    <ToolTip text="삭제를 원하시면 2~3초 동안 운동 종목을 길게 눌러주세요." />
+                </Flex>
             </PageHeader>
             <ErrorBoundary>
                 <Suspense
