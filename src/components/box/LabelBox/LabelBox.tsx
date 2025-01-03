aabea2cadcec18a1a73fea2ba2204322a38638f0
dhkim <dhkim@i2l.co.kr>
2025-01-04 00:01:26 +0900

    feat: LabelBox 수정 - labeText 속성 타입 수정정

--- src/components/box/LabelBox/LabelBox.tsx
+++ src/components/box/LabelBox/LabelBox.tsx
@@ -1,21 +1,20 @@
 import styled from "styled-components";
 
-const Container = styled.div<{ gap?: string; align?: string }>`
+const Container = styled.div<{gap?: string; align?: string}>`
     width: 100%;
     display: flex;
     flex-direction: column;
-    gap: ${({ gap }) => (gap ? gap : "10px")};
-    align-items: ${({ align }) =>
-        align === "center" ? "center" : "flex-start"};
+    gap: ${({gap}) => (gap ? gap : "10px")};
+    align-items: ${({align}) => (align === "center" ? "center" : "flex-start")};
 `;
 const Label = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.md};
-    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    font-size: ${({theme}) => theme.fontSize.md};
+    font-weight: ${({theme}) => theme.fontWeight.semibold};
 `;
 
 type LabelBoxProps = {
     children: React.ReactNode;
-    labelText: string;
+    labelText: React.ReactNode;
     gap?: string;
     align?: "center" | "start";
 };
