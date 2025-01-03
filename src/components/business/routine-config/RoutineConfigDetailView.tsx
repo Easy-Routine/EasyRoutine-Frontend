4048c9d5d897aefe4427b3cc551eb14a24f36ccc
dhkim <dhkim@i2l.co.kr>
2025-01-04 00:02:18 +0900

    feat: 루틴상세페이지 운동 없을 시 FAB에 텍스트 첨부

--- src/components/business/routine-config/RoutineConfigDetailView.tsx
+++ src/components/business/routine-config/RoutineConfigDetailView.tsx
@@ -3,16 +3,17 @@ import Box from "components/box/Box/Box";
 import RoutineConfigColorTabBottomBar from "./RoutineConfigUpdateColorTabBottomBar";
 import styled from "styled-components";
 import WorkoutConfigDetailAccordion from "../workout-config/WorkoutConfigDetailAccordion";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import useRoutineConfigOneQuery from "hooks/server/useGetRoutineConfigOneQuery";
 import RoutineConfigUpdateNameTitleText from "./RoutineConfigUpdateNameTitleText";
 import ErrorBoundary from "components/box/ErrorBoundary/ErrorBounday";
 import WorkoutLibraryListBottomSheet from "../workout-library/WorkoutLibraryListBottomSheet";
 import FloatingActionButton from "components/content/FloatingActionButton/FloatingActionButton";
 import useModal from "hooks/client/useModal";
-import { Suspense } from "react";
+import {Suspense} from "react";
 import CommonLoading from "components/content/CommonLoading/CommonLoading";
 import DefferredComponent from "components/box/DefferedComponent/DefferedComponent";
+import EmptyBoundary from "../EmptyBoundary";
 
 const Container = styled.div`
     display: flex;
@@ -21,9 +22,9 @@ const Container = styled.div`
 `;
 
 const RoutineConfigDetailView = () => {
-    const { routineConfigId } = useParams();
-    const { data: routineConfigDetail } = useRoutineConfigOneQuery(
-        routineConfigId as string
+    const {routineConfigId} = useParams();
+    const {data: routineConfigDetail} = useRoutineConfigOneQuery(
+        routineConfigId as string,
     );
     const {
         isOpen: isWorkoutLibraryBottomSheetOpen,
@@ -40,7 +41,7 @@ const RoutineConfigDetailView = () => {
             </Box>
             <Accordion.List
                 data={routineConfigDetail!.workoutConfigs}
-                render={(workoutConfig) => (
+                render={workoutConfig => (
                     <WorkoutConfigDetailAccordion
                         data={workoutConfig}
                         key={workoutConfig._id}
@@ -73,6 +74,27 @@ const RoutineConfigDetailView = () => {
             <FloatingActionButton
                 onButtonClick={() => openWorkoutLibraryListBottomSheet()}
             />
+
+            <EmptyBoundary
+                data={routineConfigDetail?.workoutConfigs}
+                fallback={
+                    <FloatingActionButton
+                        text={
+                            <>
+                                아래 버튼을 통해 원하는
+                                <br /> 운동 종목을 추가해주세요
+                            </>
+                        }
+                        onButtonClick={() =>
+                            openWorkoutLibraryListBottomSheet()
+                        }
+                    />
+                }
+            >
+                <FloatingActionButton
+                    onButtonClick={() => openWorkoutLibraryListBottomSheet()}
+                />
+            </EmptyBoundary>
 
             <RoutineConfigColorTabBottomBar
                 defaultValue={routineConfigDetail!.color}
