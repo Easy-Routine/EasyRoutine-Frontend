2c5c54a1a9263d848c30a338673310aece5d0d51
doggopawer <doggopawer@gmail.com>
2025-01-23 23:33:13 +0900

    fix: ToolTip 버튼이 아닌 말풍선 부분을 클릭해도 토글이 되는 문제 해결

--- src/components/content/ToolTip/ToolTip.tsx
+++ src/components/content/ToolTip/ToolTip.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {ReactComponent as QuestionIcon} from "assets/image/tooltip-question.svg";
+import { ReactComponent as QuestionIcon } from "assets/image/tooltip-question.svg";
 
 const Container = styled.div`
     width: 24px;
@@ -12,6 +12,7 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
 `;
+
 const ToolTipBox = styled.div<{
     isActive: boolean;
     toolTipPosition: "left" | "right";
@@ -20,55 +21,49 @@ const ToolTipBox = styled.div<{
     max-width: 120px;
     max-height: 80px;
     position: absolute;
-    ${({toolTipPosition}) =>
+    ${({ toolTipPosition }) =>
         toolTipPosition === "left" ? "left: -145px;" : "right: -145px"};
     top: 50%;
     transform: translateY(-50%);
-    background-color: ${({theme}) => theme.color.primary};
-    opacity: ${({isActive}) => (isActive ? 1 : 0)};
+    background-color: ${({ theme }) => theme.color.primary};
+    opacity: ${({ isActive }) => (isActive ? 1 : 0)};
     font-size: 8px;
     padding: 7px;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-weight: ${({theme}) => theme.fontWeight.bold};
-    color: ${({theme}) => theme.color.text.white};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    color: ${({ theme }) => theme.color.text.white};
     line-height: 12px;
-    border-radius: ${({theme}) => theme.borderRadius.xs};
+    border-radius: ${({ theme }) => theme.borderRadius.xs};
     transition: all 0.3s ease-in-out;
-
-    // 말풍선 꼬리 추가
 
     &::before {
         content: "";
         position: absolute;
-        right: 100%; /* ToolTipBox의 왼쪽에 위치 */
+        right: 100%;
         top: 50%;
         transform: translateY(-50%);
-        border-width: 6px; /* 꼬리의 크기 */
+        border-width: 6px;
         border-style: solid;
-        border-color: transparent ${({theme}) => theme.color.primary}
-            transparent transparent; /* 꼬리 색상 */
-        display: ${({toolTipPosition}) =>
-            toolTipPosition === "right"
-                ? "block"
-                : "none"}; /* 왼쪽일 때만 표시 */
+        border-color: transparent ${({ theme }) => theme.color.primary}
+            transparent transparent;
+        display: ${({ toolTipPosition }) =>
+            toolTipPosition === "right" ? "block" : "none"};
     }
 
     &::after {
         content: "";
         position: absolute;
-        left: 100%; /* ToolTipBox의 오른쪽에 위치 */
+        left: 100%;
         top: 50%;
         transform: translateY(-50%);
-        border-width: 6px; /* 꼬리의 크기 */
+        border-width: 6px;
         border-style: solid;
         border-color: transparent transparent transparent
-            ${({theme}) => theme.color.primary}; /* 꼬리 색상 */
-        display: ${({toolTipPosition}) =>
-            toolTipPosition === "left"
-                ? "block"
-                : "none"}; /* 오른쪽일 때만 표시 */
+            ${({ theme }) => theme.color.primary};
+        display: ${({ toolTipPosition }) =>
+            toolTipPosition === "left" ? "block" : "none"};
     }
 `;
 
@@ -77,17 +72,25 @@ type ToolTipProps = {
     toolTipPosition: "left" | "right";
 };
 
-const ToolTip = ({text, toolTipPosition}: ToolTipProps) => {
+const ToolTip = ({ text, toolTipPosition }: ToolTipProps) => {
     const [isActive, setIsActive] = useState(false);
 
     const handleToolTipClick = () => {
         setIsActive(prev => !prev);
     };
 
+    const handleToolTipBoxClick = (e: React.MouseEvent) => {
+        e.stopPropagation(); // 클릭 이벤트 전파 방지
+    };
+
     return (
         <Container onClick={handleToolTipClick}>
             <QuestionIcon />
-            <ToolTipBox isActive={isActive} toolTipPosition={toolTipPosition}>
+            <ToolTipBox
+                isActive={isActive}
+                toolTipPosition={toolTipPosition}
+                onClick={handleToolTipBoxClick} // ToolTipBox 클릭 이벤트
+            >
                 {text}
             </ToolTipBox>
         </Container>
