00a107ba982b0982baec615885b3af74c2f37eb5
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-15 16:50:24 +0900

    feat: CircleButton 생성 - 구조 및 스타일링 정의

---
+++ src/headful/CircleButton/CircleButton.tsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import {css, useTheme} from "@emotion/react";
+import React from "react";
+
+type CircleButtonProps = {
+    children: React.ReactNode;
+    width: number;
+    height: number;
+};
+
+const CircleButton = ({children, width, height}: CircleButtonProps) => {
+    const theme = useTheme();
+
+    const basicButtonStyle = css`
+        width: ${width}px;
+        height: ${height}px;
+        min-width: ${width}px;
+        border-radius: ${theme.borderRadius.circle};
+        background-color: ${theme.color.primary};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: ${theme.boxShadow};
+        color: ${theme.color.text.white};
+        border: none;
+    `;
+
+    return <button css={basicButtonStyle}>{children}</button>;
+};
+
+export default CircleButton;
