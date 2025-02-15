e44fcf3301d01225d6ae461862057f6b98c61e2f
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-15 23:30:17 +0900

    feat: SwipeableAccordion.stories 생성

---
+++ src/stories/new/SwipeableAccordion.stories.tsx
@@ -0,0 +1,54 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import type {Meta, StoryObj} from "@storybook/react";
+import SwipeableAccordion from "headful/SwiperableAccordion/SwipeableAccordion";
+import {css} from "@emotion/react";
+import ImageText from "headful/ImageText/ImageText";
+import ImageTextText from "headful/ImageText/ImageTextText/ImageTextText";
+
+type SwipeableAccordionProps = React.ComponentProps<typeof SwipeableAccordion>;
+
+const meta: Meta<SwipeableAccordionProps> = {
+    title: "Components/SwipeableAccordion", // Storybook에서 표시될 제목
+    component: SwipeableAccordion,
+};
+
+export default meta;
+
+type Story = StoryObj<SwipeableAccordionProps>;
+
+export const DefaultSwipeableAccordion: Story = {
+    render: () => (
+        <div
+            css={css`
+                display: flex;
+            `}
+        >
+            <SwipeableAccordion>
+                <SwipeableAccordion.Box>
+                    <SwipeableAccordion.Visible>
+                        <ImageText>
+                            <ImageText.Image
+                                width={60}
+                                height={60}
+                                src="https://example.com/image.jpg"
+                            />
+                            <ImageText.Text>
+                                <ImageTextText.Bold>
+                                    Bold Text
+                                </ImageTextText.Bold>
+                                <ImageTextText.Regular>
+                                    Regular Text
+                                </ImageTextText.Regular>
+                            </ImageText.Text>
+                        </ImageText>
+                    </SwipeableAccordion.Visible>
+                    <SwipeableAccordion.Hidden>
+                        <div>히든</div>
+                    </SwipeableAccordion.Hidden>
+                    <SwipeableAccordion.DeleteButton />
+                </SwipeableAccordion.Box>
+            </SwipeableAccordion>
+        </div>
+    ),
+};
