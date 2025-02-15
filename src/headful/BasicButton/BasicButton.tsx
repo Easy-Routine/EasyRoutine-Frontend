72c4544b5484c82d82a2acd33fd2f5f68c2f69bc
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-15 16:42:26 +0900

    feat: BasicButton 생성

---
+++ src/headful/BasicButton/BasicButton.tsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import {css, useTheme} from "@emotion/react";
+import React from "react";
+
+type BasicButtonProps = {
+    disabled?: boolean;
+    children: React.ReactNode;
+};
+
+const BasicButton = ({disabled, children}: BasicButtonProps) => {
+    const theme = useTheme();
+
+    const basicButtonStyle = css`
+        width: 100%;
+        height: 40px;
+        min-height: 40px;
+        border: none;
+        background-color: ${theme.color.primary};
+        color: ${theme.color.text.white};
+        border-radius: ${theme.borderRadius.xs};
+        font-family: "Noto Sans Korean", sans-serif;
+        opacity: ${disabled ? "0.5" : "1"};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `;
+
+    return <button css={basicButtonStyle}>{children}</button>;
+};
+
+export default BasicButton;
