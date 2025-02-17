1aecda4b022617093ffe535b42a01ce6d82b97e1
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-17 20:40:00 +0900

    chore: LineCheckBoxGroup.stories 수정 - 불필요한 코드 삭제

--- src/stories/new/LineCheckBoxGroup.stories.tsx
+++ src/stories/new/LineCheckBoxGroup.stories.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import type {Meta, StoryObj} from "@storybook/react";
 import LineCheckBoxGroup from "headful/LineCheckBoxGroup/LineCheckBoxGroup";
-import ImageText from "headful/ImageText/ImageText";
-import ImageTextText from "headful/ImageText/ImageTextText/ImageTextText";
 
 type LineCheckBoxGroupProps = React.ComponentProps<typeof LineCheckBoxGroup>;
 
@@ -19,51 +17,9 @@ type Story = StoryObj<LineCheckBoxGroupProps>;
 export const DefaultLineCheckBoxGroup: Story = {
     render: () => (
         <LineCheckBoxGroup>
-            <LineCheckBoxGroup.Item value="item1">
-                <ImageText>
-                    <ImageText.Image
-                        width={40}
-                        height={40}
-                        src="https://example.com/image.jpg"
-                    />
-                    <ImageText.Text>
-                        <ImageTextText.Bold>Bold Text</ImageTextText.Bold>
-                        <ImageTextText.Regular>
-                            Regular Text
-                        </ImageTextText.Regular>
-                    </ImageText.Text>
-                </ImageText>
-            </LineCheckBoxGroup.Item>
-            <LineCheckBoxGroup.Item value="item2">
-                <ImageText>
-                    <ImageText.Image
-                        width={40}
-                        height={40}
-                        src="https://example.com/image.jpg"
-                    />
-                    <ImageText.Text>
-                        <ImageTextText.Bold>Bold Text</ImageTextText.Bold>
-                        <ImageTextText.Regular>
-                            Regular Text
-                        </ImageTextText.Regular>
-                    </ImageText.Text>
-                </ImageText>
-            </LineCheckBoxGroup.Item>
-            <LineCheckBoxGroup.Item value="item3">
-                <ImageText>
-                    <ImageText.Image
-                        width={40}
-                        height={40}
-                        src="https://example.com/image.jpg"
-                    />
-                    <ImageText.Text>
-                        <ImageTextText.Bold>Bold Text</ImageTextText.Bold>
-                        <ImageTextText.Regular>
-                            Regular Text
-                        </ImageTextText.Regular>
-                    </ImageText.Text>
-                </ImageText>
-            </LineCheckBoxGroup.Item>
+            <LineCheckBoxGroup.Item value="item1">1</LineCheckBoxGroup.Item>
+            <LineCheckBoxGroup.Item value="item2">2</LineCheckBoxGroup.Item>
+            <LineCheckBoxGroup.Item value="item3">3</LineCheckBoxGroup.Item>
         </LineCheckBoxGroup>
     ),
 };
