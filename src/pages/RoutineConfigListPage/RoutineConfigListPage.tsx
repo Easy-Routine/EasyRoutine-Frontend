070ad648de4203097c124f80341aa390d8f65cdf
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-17 22:11:11 +0900

    feat: RoutineConfigListPage 생성

---
+++ src/pages/RoutineConfigListPage/RoutineConfigListPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoutineConfigAccordionList from "./components/RoutineConfigAccordionList";
+
+const RoutineConfigListPage = () => {
+    return (
+        <>
+            <RoutineConfigAccordionList />
+        </>
+    );
+};
+
+export default RoutineConfigListPage;
