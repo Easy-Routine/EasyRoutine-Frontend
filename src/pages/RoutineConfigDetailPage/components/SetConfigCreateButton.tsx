2fb6fbbb07a3d6160e23a79381d479b967db5665
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-24 00:27:49 +0900

    feat: SetConfigCreateButton 수정 - SetConfig 생성 기능 추가

--- src/pages/RoutineConfigDetailPage/components/SetConfigCreateButton.tsx
+++ src/pages/RoutineConfigDetailPage/components/SetConfigCreateButton.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import FlexBox from "headful/FlexBox/FlexBox";
+import {MouseEventHandler} from "react";
+import Text from "headful/Text/Text";
+import useCreateSetConfigOneMutation from "hooks/server/useCreateSetConfigOneMutation";
+import {ReactComponent as PlusIcon} from "assets/image/plus2.svg";
 
-const SetConfigCreateButton = () => {
-    return <div>SetConfigCreateButton</div>;
+type SetConfigCreateButtonButtonProps = {
+    routineConfigId: string;
+    workoutConfigId: string;
+};
+
+const SetConfigCreateButton = ({
+    routineConfigId,
+    workoutConfigId,
+}: SetConfigCreateButtonButtonProps) => {
+    const {mutateAsync: createSetConfigOneMutate} =
+        useCreateSetConfigOneMutation();
+
+    const handleSetCreateButtonClick: MouseEventHandler<
+        HTMLDivElement
+    > = async e => {
+        e.stopPropagation();
+        await createSetConfigOneMutate({
+            routineConfigId,
+            workoutConfigId,
+        });
+    };
+
+    return (
+        <FlexBox
+            gap={16}
+            alignItems="center"
+            onClick={handleSetCreateButtonClick}
+        >
+            <PlusIcon color={"var(--color-primary)"} />
+            <Text color={"var(--color-primary)"}>세트 추가하기</Text>
+        </FlexBox>
+    );
 };
 
 export default SetConfigCreateButton;
