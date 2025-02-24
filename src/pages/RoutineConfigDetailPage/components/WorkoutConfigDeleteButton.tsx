057a1c858fee478ceb61ebecad47ac11de514181
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-24 22:26:01 +0900

    feat: WorkoutConfigDeleteButton 생성

--- src/pages/RoutineConfigDetailPage/components/WorkoutConfigDeleteButton.tsx
+++ src/pages/RoutineConfigDetailPage/components/WorkoutConfigDeleteButton.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import SwipeableAccordion from "headful/SwiperableAccordion/SwipeableAccordion";
+import useDeleteWorkoutConfigOneMutation from "hooks/server/useDeleteWorkoutConfigOneMutation";
 
-const WorkoutConfigDeleteButton = () => {
-    return <div>WorkoutConfigDeleteButton</div>;
+type WorkoutConfigDeleteButtonProps = {
+    workoutConfigId: string;
+    routineConfigId: string;
+};
+
+const WorkoutConfigDeleteButton = ({
+    workoutConfigId,
+    routineConfigId,
+}: WorkoutConfigDeleteButtonProps) => {
+    const {mutateAsync: deleteWorkoutConfigOneMutate} =
+        useDeleteWorkoutConfigOneMutation();
+
+    const handleWorkoutConfigDeleteButtonClick = async () => {
+        await deleteWorkoutConfigOneMutate({
+            routineConfigId: routineConfigId as string,
+            workoutConfigId,
+        });
+    };
+
+    return (
+        <SwipeableAccordion.DeleteButton
+            onSwipeableAccordionDeleteButtonClick={
+                handleWorkoutConfigDeleteButtonClick
+            }
+        />
+    );
 };
 
 export default WorkoutConfigDeleteButton;
