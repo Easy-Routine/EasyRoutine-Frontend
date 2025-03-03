9d0f827c85f2e9d05d84dc00702dae2ec7c7bc25
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-03-03 22:33:39 +0900

    feat: RoutineConfigDetailPage 수정 - 바뀐 구조 반영

--- src/pages/RoutineConfigDetailPage/RoutineConfigDetailPage.tsx
+++ src/pages/RoutineConfigDetailPage/RoutineConfigDetailPage.tsx
@@ -1,17 +1,37 @@
-import React from "react";
-import WorkoutConfigAccordionList from "./components/WorkoutConfigAccordionList";
 import {useParams} from "react-router-dom";
+import RoutineConfigGetContainer from "./components/RoutineConfigGetContainer";
+import BottomSheetModal from "headful/BottomSheetModal/BottomSheetModal";
+import FloatingCircleButton from "headful/FloatingCircleButton/FloatingCircleButton";
+import {ReactComponent as PlusIcon} from "assets/image/plus.svg";
+import WorkoutLibraryGetContainer from "./components/WorkoutLibraryGetContainer";
 
-const RoutineConfigListPage = () => {
+const RoutineConfigDetailPage = () => {
     const {routineConfigId} = useParams();
 
     return (
         <>
-            <WorkoutConfigAccordionList
+            <RoutineConfigGetContainer
                 routineConfigId={routineConfigId as string}
             />
+            <BottomSheetModal>
+                <BottomSheetModal.Backdrop />
+                <BottomSheetModal.Content>
+                    <WorkoutLibraryGetContainer
+                        routineConfigId={routineConfigId as string}
+                    />
+                </BottomSheetModal.Content>
+                <BottomSheetModal.Trigger>
+                    <FloatingCircleButton
+                        width={64}
+                        height={64}
+                        onFloatingCircleButtonClick={() => {}}
+                    >
+                        <PlusIcon color={"var(--text-white)"} />
+                    </FloatingCircleButton>
+                </BottomSheetModal.Trigger>
+            </BottomSheetModal>
         </>
     );
 };
 
-export default RoutineConfigListPage;
+export default RoutineConfigDetailPage;
