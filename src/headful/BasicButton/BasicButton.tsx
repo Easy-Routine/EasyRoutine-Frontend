5acbf733e1efd37f750316595520e0c5dc910be8
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-03-03 22:26:46 +0900

    feat: BasicButton 수정 - Prop타입 변경

--- src/headful/BasicButton/BasicButton.tsx
+++ src/headful/BasicButton/BasicButton.tsx
@@ -1,31 +1,20 @@
-import React from "react";
+import React, {ButtonHTMLAttributes} from "react";
 import styles from "./BasicButton.module.scss";
 
-type BasicButtonProps = {
-    disabled?: boolean;
-    children: React.ReactNode;
-    onBasicButtonClick?: () => void;
-};
+type BasicButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {};
 
-const BasicButton = ({
-    disabled,
-    children,
-    onBasicButtonClick,
-}: BasicButtonProps) => {
+const BasicButton = ({...props}: BasicButtonProps) => {
     // 동적으로 변경되는 opacity는 인라인 스타일로 처리합니다.
     const dynamicStyle: React.CSSProperties = {
-        opacity: disabled ? "0.5" : "1",
+        opacity: props.disabled ? "0.5" : "1",
     };
 
     return (
         <button
             className={styles.basicButton}
-            onClick={onBasicButtonClick}
-            disabled={disabled}
-            style={dynamicStyle}
-        >
-            {children}
-        </button>
+            {...props}
+            style={{...dynamicStyle, ...props.style}}
+        />
     );
 };
 
