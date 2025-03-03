81de73467ad64960f96535406fc617806a50796d
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-03-03 22:33:17 +0900

    feat: WorkoutLibraryGetContainer 및 하위 컴포넌트 생성

---
+++ src/pages/RoutineConfigDetailPage/components/WorkoutLibraryCreateContainer/WorkoutLibraryCreateContainer.tsx
@@ -0,0 +1,48 @@
+import LineCheckBoxGroup from "headful/LineCheckBoxGroup/LineCheckBoxGroup";
+import React, {useEffect, useState} from "react";
+import WorkoutLibraryCheckBoxItemList from "../WorkoutLibraryCheckBoxItemList";
+import {WorkoutLibrary} from "types/model";
+import WorkoutConfigCreateButton from "../WorkoutConfigCreateButton";
+import useCreateWorkoutConfigAllMutation from "hooks/server/useCreateWorkoutConfigAllMutation";
+import {useModal} from "headless/Modal/Modal";
+
+type WorkoutLibraryCreateContainerProps = {
+    workoutLibraries: WorkoutLibrary[];
+    routineConfigId: string;
+};
+
+const WorkoutLibraryCreateContainer = ({
+    workoutLibraries,
+    routineConfigId,
+}: WorkoutLibraryCreateContainerProps) => {
+    const [workoutLibraryIds, setWorkoutLibraryIds] = useState<string[]>([]);
+    const {closeModal} = useModal();
+
+    const {mutateAsync: createWorkoutConfigAllMutation} =
+        useCreateWorkoutConfigAllMutation();
+
+    const handleBasicButtonClick = async () => {
+        await createWorkoutConfigAllMutation({
+            workoutLibraryIds,
+            routineConfigId,
+        });
+
+        closeModal();
+    };
+
+    const handleCheckBoxItemClick = (value: string[]) => {
+        setWorkoutLibraryIds(value);
+    };
+
+    return (
+        <LineCheckBoxGroup defaultValue={workoutLibraryIds}>
+            <WorkoutLibraryCheckBoxItemList
+                workoutLibraries={workoutLibraries}
+                onCheckBoxItemClick={handleCheckBoxItemClick}
+            />
+            <WorkoutConfigCreateButton onButtonClick={handleBasicButtonClick} />
+        </LineCheckBoxGroup>
+    );
+};
+
+export default WorkoutLibraryCreateContainer;
