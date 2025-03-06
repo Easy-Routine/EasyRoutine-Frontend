338a71f8bde1c2d7a239c9c86cb75295072566d9
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-03-06 21:50:21 +0900

    feat: NavigateContainer

---
+++ src/components/NavigateContainer.tsx
@@ -0,0 +1,56 @@
+import BottomBox from "headful/BottomBox/BottomBox";
+import React from "react";
+import {ReactComponent as HomeIcon} from "assets/image/home.svg";
+import {ReactComponent as GraphIcon} from "assets/image/graph.svg";
+import {ReactComponent as TileIcon} from "assets/image/tile.svg";
+import {ReactComponent as UserIcon} from "assets/image/user.svg";
+import TabGroup from "headless/TabGroup/TabGroup";
+import ROUTES from "constants/routes";
+import {useNavigate} from "react-router-dom";
+import FlexBox from "headful/FlexBox/FlexBox";
+import IconTabGroup from "headful/IconTabGroup/IconTabGroup";
+
+type NaviationBoxProps = {
+    path: string;
+};
+
+const NavigateContainer = ({path}: NaviationBoxProps) => {
+    const navigate = useNavigate();
+
+    const handleNavigationItemClick = (value: string) => {
+        navigate(value);
+    };
+
+    return (
+        <BottomBox>
+            <IconTabGroup defaultValue={path}>
+                <IconTabGroup.Item
+                    value={ROUTES.CONFIG.LIST.PATH}
+                    onTabGroupItemClick={handleNavigationItemClick}
+                    icon={<HomeIcon />}
+                    label="홈"
+                />
+                <IconTabGroup.Item
+                    value={ROUTES.RECORD.LIST.PATH}
+                    onTabGroupItemClick={handleNavigationItemClick}
+                    icon={<GraphIcon />}
+                    label="기록"
+                />
+                <IconTabGroup.Item
+                    value={ROUTES.LIBRARY.PATH}
+                    onTabGroupItemClick={handleNavigationItemClick}
+                    icon={<TileIcon />}
+                    label="라이브러리"
+                />
+                <IconTabGroup.Item
+                    value={ROUTES.MY.PATH}
+                    onTabGroupItemClick={handleNavigationItemClick}
+                    icon={<UserIcon />}
+                    label="마이"
+                />
+            </IconTabGroup>
+        </BottomBox>
+    );
+};
+
+export default NavigateContainer;
