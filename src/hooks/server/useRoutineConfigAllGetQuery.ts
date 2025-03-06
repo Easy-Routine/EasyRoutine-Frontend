9a1a68fc2c29128a017f9bd03ef5dc0ea3410173
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-03-06 22:19:54 +0900

    rename: useGetRoutineConfigAllQuery => useRoutineConfigAllGetQuery 로 이름 변경

---
+++ src/hooks/server/useRoutineConfigAllGetQuery.ts
@@ -0,0 +1,18 @@
+import {useSuspenseQuery} from "@tanstack/react-query";
+import {getRoutineConfigAll} from "services/routine-config";
+import queryKey from "constants/queryKeys";
+
+const useRoutineConfigAllGetQuery = () => {
+    return useSuspenseQuery({
+        queryKey: [queryKey.getRoutineConfigAll],
+        queryFn: async () => {
+            const response = await getRoutineConfigAll();
+            return response;
+        },
+        select: response => ({
+            routineConfigs: response,
+        }),
+    });
+};
+
+export default useRoutineConfigAllGetQuery;
