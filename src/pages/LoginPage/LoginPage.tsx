3bf2af475fd8d174a19bbc9190ca596c866858cf
doggopawer <doggopawer@gmail.com>
2025-04-12 18:44:19 +0900

    chore: LoginPage 수정 - 주석 삭제

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -6,64 +6,7 @@ import Logo from "components/content/Logo/Logo";
 import Flex from "headful/Flex/Flex";
 import Text from "headful/Text/Text";
 
-// const Container = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     gap: 20px;
-//     height: 100vh;
-//     position: relative;
-// `;
 
-// const TitleBox = styled.div`
-//     position: absolute;
-//     top: 40%; /* 화면의 10% 위치에 배치 */
-//     left: 50%; /* 화면의 중앙에 배치 */
-//     transform: translateX(-50%); /* 중앙 정렬을 위해 X축으로 이동 */
-
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     gap: 12px;
-// `;
-// const Description = styled.div`
-//     text-align: center;
-//     line-height: 16px;
-//     font-size: ${({theme}) => theme.fontSize.md};
-// `;
-
-// const ButtonBox = styled.div`
-//     width: 90%;
-//     position: absolute;
-//     bottom: 20%; /* 화면의 하단에서 10% 위치에 배치 */
-//     left: 50%; /* 화면의 중앙에 배치 */
-//     transform: translateX(-50%); /* 중앙 정렬을 위해 X축으로 이동 */
-//     display: flex;
-//     flex-direction: column;
-//     gap: 12px;
-// `;
-
-// const AppleButton = styled(Button)`
-//     border: none;
-//     color: ${({theme}) => theme.color.text.white};
-//     background-color: ${({theme}) => theme.color.text.black};
-// `;
-
-// const GoogleButton = styled(Button)`
-//     border: 1px solid #bfbfbf;
-// `;
-
-// const AppleLogo = styled(AppleLogoIcon)`
-//     width: 20px;
-//     height: 20px;
-//     position: absolute;
-//     left: 16px;
-//     fill: ${({theme}) => theme.color.text.white};
-// `;
-
-// const GoogleLogo = styled(GoogleLogoIcon)`
-//     position: absolute;
-//     left: 16px;
-// `;
 
 const LoginPage = () => {
     return (
