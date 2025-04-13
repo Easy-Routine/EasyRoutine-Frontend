d3a23c2c3b2638a47260dd38ebdf385f1c0e5753
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-04-13 21:21:51 +0900

    feat: PrivateRoute 수정 - 쿼리파라미터의 토큰을 로컬스토리지에 저장하는 로직 구현

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,6 +1,11 @@
 import ROUTES from "constants/routes";
 import {useEffect} from "react";
-import {Outlet, useLocation, useNavigate} from "react-router-dom";
+import {
+    Outlet,
+    useLocation,
+    useNavigate,
+    useSearchParams,
+} from "react-router-dom";
 import {useQueryClient} from "@tanstack/react-query";
 import queryKey from "constants/queryKeys";
 import {getContext} from "services/auth";
@@ -10,11 +15,23 @@ const PrivateRoute = () => {
     const navigate = useNavigate();
     const currentPath = location.pathname;
     const queryClient = useQueryClient();
+    const [searchParams, setSearchParams] = useSearchParams();
 
     useEffect(() => {
         (async () => {
             try {
                 console.log("Private Route!");
+
+                if (searchParams.has("token")) {
+                    // 토큰을 제거합니다.
+
+                    const token = searchParams.get("token");
+                    localStorage.setItem("accessToken", token as string);
+                    searchParams.delete("token");
+                    // 변경된 searchParams를 반영하여 URL을 업데이트합니다.
+                    setSearchParams(searchParams, {replace: true});
+                }
+
                 // 'context'라는 queryKey로 데이터를 prefetch 합니다.
                 // await queryClient.fetchQuery({
                 //     queryKey: [queryKey.getContext],
