b6e13cd89345cf0027392e262d323d3b8efefd21
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-04-13 22:42:01 +0900

    feat: Image 수정 - borderRadius 추가

--- src/headful/Image/Image.tsx
+++ src/headful/Image/Image.tsx
@@ -4,14 +4,15 @@ import styles from "./Image.module.scss";
 type ImageProps = {
     width: number;
     height: number;
+    borderRadius?: string;
     src: string;
 };
 
-const Image = ({width, height, src}: ImageProps) => {
+const Image = ({width, height, borderRadius = "0px", src}: ImageProps) => {
     return (
         <img
             className={styles.image}
-            style={{width: `${width}px`, height: `${height}px`}}
+            style={{width: `${width}px`, height: `${height}px`, borderRadius}}
             src={src}
             alt=""
         />
