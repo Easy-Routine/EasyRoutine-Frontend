51ed8cd2ac8dcdc43bacd096d5832fd9044392bf
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-04-13 22:42:53 +0900

    feat: MyPage 수정 - 퍼블리싱 작업 완료

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,10 +1,17 @@
 import NavigateBottomBox from "components/NavigateBottomBox";
+import Flex from "headful/Flex/Flex";
 import LogoArea from "headful/LogoArea/LogoArea";
+import OutlinedButton from "headful/OutlinedButton/OutlinedButton";
 import Footer from "headful/PrivatePageTemplate/Footer/Footer";
 import Header from "headful/PrivatePageTemplate/Header/Header";
 import Main from "headful/PrivatePageTemplate/Main/Main";
 import PrivatePageTemplate from "headful/PrivatePageTemplate/PrivatePageTemplate";
+import ProfileBox from "headful/ProfileBox/ProfileBox";
+import UnderlineItem from "headful/UnderlineItem/UnderlineItem";
 import {useLocation} from "react-router-dom";
+import {CiUser} from "react-icons/ci";
+import {IoIosArrowForward} from "react-icons/io";
+import Text from "headful/Text/Text";
 
 const MyPage = () => {
     const location = useLocation();
@@ -14,75 +21,26 @@ const MyPage = () => {
                 <LogoArea />
             </Header>
             <Main>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
-                <div>메인</div>
+                <Flex padding={20} flexDirection="column" gap={30}>
+                    <ProfileBox imageSrc="" name="김동현" />
+                    <OutlinedButton style={{alignSelf: "center"}}>
+                        로그아웃
+                    </OutlinedButton>
+                    <UnderlineItem>
+                        <Flex
+                            justifyContent="space-between"
+                            alignItems="center"
+                        >
+                            <Flex gap={10} alignItems="center">
+                                <CiUser size={24} />
+                                <Text>문의사항</Text>
+                            </Flex>
+                            <IoIosArrowForward size={24} />
+                        </Flex>
+                    </UnderlineItem>
+                </Flex>
             </Main>
             <Footer>
-                {/* 푸터 */}
                 <NavigateBottomBox path={location.pathname} />
             </Footer>
         </PrivatePageTemplate>
