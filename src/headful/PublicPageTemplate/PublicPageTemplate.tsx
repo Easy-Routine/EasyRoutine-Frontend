35c1038e6ba1f2783e0e1ee7db64322a93b2b976
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-04-13 21:29:21 +0900

    feat: PublicPageTemplate 수정 - Outlet 삭제 - fixed 삭제

--- src/headful/PublicPageTemplate/PublicPageTemplate.tsx
+++ src/headful/PublicPageTemplate/PublicPageTemplate.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import {Outlet} from "react-router-dom";
 import styles from "./PublicPageTemplate.module.scss";
 
-const PublicPageTemplate = () => {
-    return (
-        <div className={styles.PublicPageTemplate}>
-            <Outlet />
-        </div>
-    );
+type PublicPageTemplateProps = {
+    children: React.ReactNode;
+};
+
+const PublicPageTemplate = ({children}: PublicPageTemplateProps) => {
+    return <div className={styles.PublicPageTemplate}>{children}</div>;
 };
 
 export default PublicPageTemplate;
