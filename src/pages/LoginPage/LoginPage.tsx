16d285899ab8cd4cb1bfa41903886a472ae5b497
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-04-13 21:30:10 +0900

    feat: LoginPage 수정 - PublicPageTemplate 적용

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -5,33 +5,38 @@ import NaverButton from "./components/NaverButton/NaverButton";
 import Logo from "components/content/Logo/Logo";
 import Flex from "headful/Flex/Flex";
 import Text from "headful/Text/Text";
-
-
+import PublicPageTemplate from "headful/PublicPageTemplate/PublicPageTemplate";
 
 const LoginPage = () => {
     return (
-        <Flex
-            flexDirection="column"
-            height="100%"
-            justifyContent="flex-end"
-        >
-            <Flex flexDirection="column" height='70%' justifyContent="space-around">
-                <Flex flexDirection="column" alignItems="center">
-                    <Logo type="large" />
-                    <Text fontSize={14} textAlign="center">
-                        간단하고 편리한 운동을 위한
-                        <br /> 당신의 헬스 메이트
-                    </Text>
-                </Flex>
+        <PublicPageTemplate>
+            <Flex
+                flexDirection="column"
+                height="100%"
+                justifyContent="flex-end"
+            >
+                <Flex
+                    flexDirection="column"
+                    height="70%"
+                    justifyContent="space-around"
+                >
+                    <Flex flexDirection="column" alignItems="center">
+                        <Logo type="large" />
+                        <Text fontSize={14} textAlign="center">
+                            간단하고 편리한 운동을 위한
+                            <br /> 당신의 헬스 메이트
+                        </Text>
+                    </Flex>
 
-                <Flex flexDirection="column" padding={40} gap={16}>
-                    <GoogleButton/>
-                    <AppleButton />
-                    <KaKaoButton/>
-                    <NaverButton/>
+                    <Flex flexDirection="column" padding={40} gap={16}>
+                        <GoogleButton />
+                        <AppleButton />
+                        <KaKaoButton />
+                        <NaverButton />
+                    </Flex>
                 </Flex>
             </Flex>
-        </Flex>
+        </PublicPageTemplate>
     );
 };
 
