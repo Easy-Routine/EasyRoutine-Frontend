357395ffa7c9cd26ca45829446c7c968a80971eb
doggopawer <doggopawer@gmail.com>
2025-06-23 19:54:09 +0900

    feat: getRoutineHistoryAllMonthly 생성

--- src/services/routine-history.ts
+++ src/services/routine-history.ts
@@ -1,6 +1,11 @@
 import {RoutineHistory} from "types/model";
 import {Color} from "types/enum";
 import {handleError} from "utils/handleError";
+import {
+    RoutineHistoryAllGetDailyRes,
+    RoutineHistoryAllGetMonthlyReq,
+    RoutineHistoryAllGetMonthlyRes,
+} from "types/routine-history";
 
 // 확인: 완료
 export const createRoutineHistoryOne = async ({
@@ -21,13 +26,131 @@ export const createRoutineHistoryOne = async ({
     }
 };
 // 확인: 완료
-export const getRoutineHistoryAllMonthly = async ({
-    date,
-}: {
-    date: Date;
-}): Promise<any[] | undefined> => {
+export const getRoutineHistoryAllMonthly = async (
+    routineHistoryAllGetMonthlyReq: RoutineHistoryAllGetMonthlyReq,
+): Promise<RoutineHistoryAllGetMonthlyRes | undefined> => {
     try {
-        return undefined;
+        return [
+            {
+                id: 1,
+                name: "Morning Routine",
+                color: Color.VIOLET, // 가정: Color enum에서 'red'를 사용
+                workoutTime: 3600,
+                createdAt: "2025-06-01T08:00:00Z",
+                routineExercises: [
+                    {
+                        id: 1,
+                        order: 1,
+                        sets: [
+                            {
+                                id: 1,
+                                order: 1,
+                                weight: 50,
+                                rep: 10,
+                                restSec: 60,
+                                exerciseSec: 30,
+                            },
+                        ],
+                        exercise: {
+                            id: 1,
+                            name: "덤벨프레스",
+                            image: null,
+                            category: "CHEST",
+                            types: ["WEIGHT", "COUNT"],
+                            isEditable: 1,
+                            shareLevel: 1,
+                        },
+                    },
+                    {
+                        id: 1,
+                        order: 1,
+                        sets: [
+                            {
+                                id: 1,
+                                order: 1,
+                                weight: 50,
+                                rep: 10,
+                                restSec: 60,
+                                exerciseSec: 30,
+                            },
+                        ],
+                        exercise: {
+                            id: 1,
+                            name: "덤벨프레스",
+                            image: null,
+                            category: "CHEST",
+                            types: ["WEIGHT", "COUNT"],
+                            isEditable: 1,
+                            shareLevel: 1,
+                        },
+                    },
+                ],
+            },
+            {
+                id: 3,
+                name: "Evening Routine",
+                color: Color.BLUE, // 가정: Color enum에서 'blue'를 사용
+                workoutTime: 3600,
+                createdAt: "2025-06-10T08:00:00Z",
+                routineExercises: [
+                    {
+                        id: 1,
+                        order: 1,
+                        sets: [
+                            {
+                                id: 2,
+                                order: 1,
+                                weight: 60,
+                                rep: 8,
+                                restSec: 90,
+                                exerciseSec: 40,
+                            },
+                        ],
+                        exercise: {
+                            id: 1,
+                            name: "덤벨프레스",
+                            image: null,
+                            category: "CHEST",
+                            types: ["WEIGHT", "COUNT"],
+                            isEditable: 1,
+                            shareLevel: 1,
+                        },
+                    },
+                ],
+            },
+            {
+                id: 4,
+                name: "Cardio Routine",
+                color: Color.GREEN, // 가정: Color enum에서 'green'을 사용
+                workoutTime: 3600,
+                createdAt: "2025-06-20T08:00:00Z",
+                routineExercises: [
+                    {
+                        id: 4,
+                        order: 1,
+                        sets: [
+                            {
+                                id: 3,
+                                order: 1,
+                                weight: 0,
+                                rep: 0,
+                                restSec: 0,
+                                exerciseSec: 300,
+                            },
+                        ],
+                        exercise: {
+                            id: 3,
+                            name: "덤벨프레스",
+                            image: null,
+                            category: "CHEST",
+                            types: ["WEIGHT", "COUNT"],
+                            isEditable: 1,
+                            shareLevel: 1,
+                        },
+                    },
+                ],
+            },
+        ];
     } catch (e) {
         handleError(e);
     }
@@ -38,7 +161,7 @@ export const getRoutineHistoryAllDaily = async ({
     date,
 }: {
     date: Date;
-}): Promise<RoutineHistory[] | undefined> => {
+}): Promise<RoutineHistoryAllGetDailyRes[] | undefined> => {
     try {
         return undefined;
     } catch (error) {
