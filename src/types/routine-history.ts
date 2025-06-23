f951f108eb27e472d9cf4e2a1104977dbf7f0177
doggopawer <doggopawer@gmail.com>
2025-06-23 19:54:33 +0900

    feat: RoutineHistory 수정 - createdAt 생성

---
+++ src/types/routine-history.ts
@@ -0,0 +1,14 @@
+import {Routine, RoutineHistory} from "./model";
+
+export type RoutineHistoryAllGetMonthlyReq = {date: Date};
+
+export type RoutineHistoryAllGetMonthlyRes = RoutineHistory[];
+export type RoutineHistoryAllGetDailyRes = RoutineHistory[];
+
+export type RoutineHistoryDeleteReq = {
+    id: string | number;
+};
+
+export type RoutineHistoryCreateReq = Omit<Routine, "id">;
+
+export type RoutineHistoryUpdateReq = Routine;
