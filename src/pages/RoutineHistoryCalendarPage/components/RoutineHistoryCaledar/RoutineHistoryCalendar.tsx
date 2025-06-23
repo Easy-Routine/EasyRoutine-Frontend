8a0b64e9501bd96ca654964ae3e195a55f678ca4
doggopawer <doggopawer@gmail.com>
2025-06-23 09:18:27 +0900

    feat: RoutineHistoryCalendar 생성

---
+++ src/pages/RoutineHistoryCalendarPage/components/RoutineHistoryCaledar/RoutineHistoryCalendar.tsx
@@ -0,0 +1,138 @@
+import React, {useState} from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from "./RoutineHistoryCalendar.module.scss";
+import Box from "components/box/Box/Box";
+import {ReactComponent as CalendarArrowLeftIcon} from "assets/image/calendar-arrow-left.svg";
+import {ReactComponent as CalendarArrowRightIcon} from "assets/image/calendar-arrow-right.svg";
+import moment from "moment";
+import "moment/locale/ko";
+
+type DotData = {
+    _id: number;
+    color: string;
+};
+
+export type DotDataByDate = {
+    date: string;
+    [key: string]: string | DotData[];
+};
+
+type CustomCalendarProps = {
+    // onPrevMonthButtonClick: (date: any) => void;
+    // onNextMonthButtnClick: (date: any) => void;
+    // onDateButtonClick: (date: any) => void;
+    // currentMonth: Date;
+    // dotDataKey: string;
+};
+
+const RoutineHistoryCalendar = (
+    {
+        // onPrevMonthButtonClick,
+        // onNextMonthButtnClick,
+        // onDateButtonClick,
+        // currentMonth,
+        // dotDataKey,
+    }: CustomCalendarProps,
+) => {
+    const [date, setDate] = useState(new Date());
+    const [activeStartDate, setActiveStartDate] = useState(new Date());
+
+    // const {data: routineRecordAllMonthly} =
+    //     useGetRoutineRecordAllMonthlyQuery(currentMonth);
+
+    // const dotDataByDate = routineRecordAllMonthly!;
+
+    const handleDateButtonClick = (date: any) => {
+        // onDateButtonClick(date);
+        setDate(date);
+    };
+
+    const handlePrevMonthButtonClick = () => {
+        const previousMonth = new Date(
+            activeStartDate.getFullYear(),
+            activeStartDate.getMonth() - 1,
+            1,
+        );
+        // onPrevMonthButtonClick(previousMonth);
+        setActiveStartDate(previousMonth);
+    };
+
+    const handleNextMonthButtnClick = () => {
+        const nextMonth = new Date(
+            activeStartDate.getFullYear(),
+            activeStartDate.getMonth() + 1,
+            1,
+        );
+        // onNextMonthButtnClick(nextMonth);
+        setActiveStartDate(nextMonth);
+    };
+
+    const formatDate = (date: Date) => {
+        moment.locale("ko");
+        return moment(date).format("YYYY년 M월");
+    };
+
+    // const getDotDataForDate = (date: Date, dotDataKey: string) => {
+    //     const formattedDate = moment(date).format("YYYY-MM-DD");
+    //     const record = dotDataByDate.find(item => item.date === formattedDate);
+    //     return record ? record[dotDataKey] : [];
+    // };
+
+    // const tileContent = ({date: tileDate}: {date: Date}) => {
+    //     const isActive = moment(date).isSame(moment(tileDate), "day");
+    //     const isToday = moment(tileDate).isSame(moment(), "day");
+    //     const dotData = getDotDataForDate(tileDate, dotDataKey);
+    //     return (
+    //         <>
+    //             <div
+    //                 className={`${styles.circle} ${
+    //                     isActive ? styles.active : isToday ? styles.today : ""
+    //                 }`}
+    //             >
+    //                 {tileDate.getDate()}
+    //             </div>
+    //             <div className={styles.dotWrapper}>
+    //                 {Array.isArray(dotData) &&
+    //                     dotData
+    //                         .map(item => (
+    //                             <div
+    //                                 key={item._id}
+    //                                 className={styles.dot}
+    //                                 style={{backgroundColor: item.color}}
+    //                             />
+    //                         ))
+    //                         .splice(0, 3)}
+    //             </div>
+    //         </>
+    //     );
+    // };
+
+    return (
+        <Box>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <span className={styles.dateInfo}>
+                        {formatDate(activeStartDate)}
+                    </span>
+                    <div className={styles.navButtons}>
+                        <CalendarArrowLeftIcon
+                            onClick={handlePrevMonthButtonClick}
+                        />
+                        <CalendarArrowRightIcon
+                            onClick={handleNextMonthButtnClick}
+                        />
+                    </div>
+                </div>
+                <Calendar
+                    onChange={handleDateButtonClick}
+                    value={date}
+                    activeStartDate={activeStartDate}
+                    // tileContent={tileContent}
+                />
+            </div>
+        </Box>
+    );
+};
+
+export default RoutineHistoryCalendar;
