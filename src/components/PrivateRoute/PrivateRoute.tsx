2e9cb1088dda88e06d86c9ba77e701c1f3e5f7f6
doggopawer <doggopawer@gmail.com>
2025-06-29 14:41:16 +0900

    feat: PrivateRoute 수정 - 주석처리

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -33,17 +33,17 @@ const PrivateRoute = () => {
                 }
 
                 // 'context'라는 queryKey로 데이터를 prefetch 합니다.
-                // await queryClient.fetchQuery({
-                //     queryKey: [queryKey.getContext],
-                //     queryFn: async () => {
-                //         const response = await getContext();
-                //         console.log("프리페치", response);
-                //         return response;
-                //     },
-                // });
+                await queryClient.fetchQuery({
+                    queryKey: [queryKey.getContext],
+                    queryFn: async () => {
+                        const response = await getContext();
+                        console.log("프리페치", response);
+                        return response;
+                    },
+                });
             } catch (e) {
                 // 에러 발생 시 로그인 페이지로 이동합니다.
-                navigate(ROUTES.LOGIN.PATH);
+                // navigate(ROUTES.LOGIN.PATH);
             }
         })();
     }, [currentPath]);
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 // Axios 인스턴스 생성
-import axios, { AxiosInstance } from "axios";
+import axios, {AxiosInstance} from "axios";
 export type APIContextType = {
     api: AxiosInstance;
 };
@@ -10,27 +10,27 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(
-    (config) => {
+    config => {
         const token = localStorage.getItem("accessToken");
         if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
         }
         return config;
     },
-    (error) => Promise.reject(error)
+    error => Promise.reject(error),
 );
 
 // 응답 인터셉터 추가
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
+    response => response,
+    error => {
         if (error.response && error.response.status === 401) {
             localStorage.removeItem("accessToken"); // 토큰 삭제
             localStorage.removeItem("userId");
             window.dispatchEvent(new Event("accessTokenChanged")); // 이벤트 발생
         }
         return Promise.reject(error);
-    }
+    },
 );
 
 export default api;
